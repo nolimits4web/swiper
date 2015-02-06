d9abe809c9c5eaecdef1c1255e8bd43644c4f598
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-02-06 17:35:13 +0300

    Release

--- README.md
+++ README.md
@@ -1,3 +1,60 @@
-##Swiper 3
+[![Build Status](https://travis-ci.org/nolimits4web/Swiper.svg?branch=master)](https://travis-ci.org/nolimits4web/Swiper)
+[![devDependency Status](https://david-dm.org/nolimits4web/swiper/dev-status.svg)](https://david-dm.org/nolimits4web/swiper#info=devDependencies)
+[![Flattr this git repo](http://api.flattr.com/button/flattr-badge-large.png)](https://flattr.com/submit/auto?user_id=nolimits4web&url=https://github.com/nolimits4web/swiper/&title=Framework7&language=JavaScript&tags=github&category=software)
 
-Work in progress
+Swiper
+==========
+
+Swiper - is the free and ultra lightweight mobile touch slider with hardware accelerated transitions (where supported) and amazing native behavior. It is intended to use in mobile websites, mobile web apps, and mobile native apps. Designed mostly for iOS, but also works on Android and latest Desktop browsers. Swiper is created by iDangero.us
+
+## Getting Started
+  * [Getting Started Guide](http://www.idangero.us/swiper/get-started/)
+
+## Dist vs Build versions
+
+On production use files (JS and CSS) only from `dist/` folder, there will be the most stable versions, `build/` folder is only for development purpose
+
+## Build
+
+Swiper uses `gulp` to build a development (build) and dist versions.
+
+First you need to have `gulp-cli` which you should install globally.
+
+```
+$ npm install --global gulp
+```
+
+Then install all dependencies, in repo's root:
+
+```
+$ npm install
+```
+
+And build development version of Swiper:
+```
+$ gulp build
+```
+
+The result is available in `build/` folder.
+
+## Dist/Release
+
+After you have made build:
+
+```
+$ gulp dist
+```
+
+Distributable version will available in `dist/` folder.
+
+## Contributing
+
+All changes should be commited to `src/` files. Swiper uses LESS for CSS compliations, and concatenated JS files (look at gulpfile.js for concat files order)
+
+## Forum
+
+If you have questions about Swiper or want to help others you are welcome to special forum at http://www.idangero.us/swiper/forum/
+
+## Docs
+
+Documentation available at http://idangero.us/swiper/api/
--- dist/js/swiper.jquery.js
+++ dist/js/swiper.jquery.js
@@ -10,7 +10,7 @@
  * 
  * Licensed under GPL & MIT
  * 
- * Released on: January 31, 2015
+ * Released on: February 6, 2015
  */
 (function () {
     'use strict';
@@ -102,6 +102,9 @@
             loop: false,
             loopAdditionalSlides: 0,
             loopedSlides: null,
+            // Control
+            control: undefined,
+            controlInverse: false,
             // Swiping/no swiping
             allowSwipeToPrev: true,
             allowSwipeToNext: true,
@@ -1376,8 +1379,8 @@
             if (s.params.scrollbar && s.scrollbar) {
                 s.scrollbar.setTransition(duration);
             }
-            if (s.params.control && s.controller && s.params.control !== byController) {
-                s.controller.setTransition(duration);
+            if (s.params.control && s.controller) {
+                s.controller.setTransition(duration, byController);
             }
         };
         s.setWrapperTranslate = function (translate, updateActiveIndex, byController) {
@@ -1399,8 +1402,8 @@
             if (s.params.scrollbar && s.scrollbar) {
                 s.scrollbar.setTranslate(s.translate);
             }
-            if (s.params.control && s.controller && s.params.control !== byController) {
-                s.controller.setTranslate(s.translate);
+            if (s.params.control && s.controller) {
+                s.controller.setTranslate(s.translate, byController);
             }
             if (s.params.hashnav && s.hashnav) {
                 s.hashnav.setHash();
@@ -1874,24 +1877,48 @@
           Controller
           ===========================*/
         s.controller = {
-            setTranslate: function () {
+            setTranslate: function (translate, byController) {
                 var controlled = s.params.control;
-                if (!controlled instanceof Swiper) {
-                    return;
+                var multiplier, controlledTranslate;
+                if (s.isArray(controlled)) {
+                    for (var i = 0; i < controlled.length; i++) {
+                        if (controlled[i] !== byController && controlled[i] instanceof Swiper) {
+                            translate = controlled[i].rtl && controlled[i].params.direction === 'horizontal' ? -s.translate : s.translate;
+                            multiplier = (controlled[i].maxTranslate() - controlled[i].minTranslate()) / (s.maxTranslate() - s.minTranslate());
+                            controlledTranslate = (translate - s.minTranslate()) * multiplier + controlled[i].minTranslate();
+                            if (s.params.controlInverse) {
+                                controlledTranslate = controlled[i].maxTranslate() - controlledTranslate;
+                            }
+                            controlled[i].updateProgress(controlledTranslate);
+                            controlled[i].setWrapperTranslate(controlledTranslate, false, s);
+                            controlled[i].updateActiveIndex();
+                        }
+                    }
                 }
-                var translate = controlled.rtl && controlled.params.direction === 'horizontal' ? -s.translate : s.translate;
-                var multiplier = (controlled.maxTranslate() - controlled.minTranslate()) / (s.maxTranslate() - s.minTranslate());
-                var controlledTranslate = (translate - s.minTranslate()) * multiplier + controlled.minTranslate();
-                controlled.updateProgress(controlledTranslate);
-                controlled.setWrapperTranslate(controlledTranslate, false, s);
-                controlled.updateActiveIndex();
+                else if (controlled instanceof Swiper && byController !== controlled) {
+                    translate = controlled.rtl && controlled.params.direction === 'horizontal' ? -s.translate : s.translate;
+                    multiplier = (controlled.maxTranslate() - controlled.minTranslate()) / (s.maxTranslate() - s.minTranslate());
+                    controlledTranslate = (translate - s.minTranslate()) * multiplier + controlled.minTranslate();
+                    if (s.params.controlInverse) {
+                        controlledTranslate = controlled.maxTranslate() - controlledTranslate;
+                    }
+                    controlled.updateProgress(controlledTranslate);
+                    controlled.setWrapperTranslate(controlledTranslate, false, s);
+                    controlled.updateActiveIndex();
+                }
             },
-            setTransition: function (duration) {
+            setTransition: function (duration, byController) {
                 var controlled = s.params.control;
-                if (!controlled instanceof Swiper) {
-                    return;
+                if (s.isArray(controlled)) {
+                    for (var i = 0; i < controlled.length; i++) {
+                        if (controlled[i] !== byController && controlled[i] instanceof Swiper) {
+                            controlled[i].setWrapperTransition(duration, s);
+                        }
+                    }
                 }
-                controlled.setWrapperTransition(duration, s);
+                else if (controlled instanceof Swiper && byController !== controlled) {
+                    controlled.setWrapperTransition(duration, s);
+                }
             }
         };
 
