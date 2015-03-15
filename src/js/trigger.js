51506d7c482059778584aea972278b69a4c842f0
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-03-15 22:21:58 +0300

    s.triggerEvents -> s.triggerEventListeners

--- src/js/trigger.js
+++ src/js/trigger.js
@@ -1,7 +1,7 @@
 /*=========================
   Events/Callbacks/Plugins Trigger
   ===========================*/
-s.triggerEvents = {
+s.triggerEventListeners = {
 
 };
 s.trigger = function (eventName) {
@@ -11,29 +11,29 @@ s.trigger = function (eventName) {
     }
     var i;
     // Trigger events
-    if (s.triggerEvents[eventName]) {
-        for (i = 0; i < s.triggerEvents[eventName].length; i++) {
-            s.triggerEvents[eventName][i](arguments[1], arguments[2], arguments[3], arguments[4], arguments[5]);
+    if (s.triggerEventListeners[eventName]) {
+        for (i = 0; i < s.triggerEventListeners[eventName].length; i++) {
+            s.triggerEventListeners[eventName][i](arguments[1], arguments[2], arguments[3], arguments[4], arguments[5]);
         }
     }
     // Trigger plugins
     if (s.callPlugins) s.callPlugins(eventName, arguments[1], arguments[2], arguments[3], arguments[4], arguments[5]);
 };
 s.on = function (eventName, handler) {
-    if (!s.triggerEvents[eventName]) s.triggerEvents[eventName] = [];
-    s.triggerEvents[eventName].push(handler);
+    if (!s.triggerEventListeners[eventName]) s.triggerEventListeners[eventName] = [];
+    s.triggerEventListeners[eventName].push(handler);
     return s;
 };
 s.off = function (eventName, handler) {
     var i;
     if (typeof handler === 'undefined') {
         // Remove all handlers for such event
-        s.triggerEvents[eventName] = [];
+        s.triggerEventListeners[eventName] = [];
         return s;
     }
-    if (!s.triggerEvents[eventName] || s.triggerEvents[eventName].length === 0) return;
-    for (i = 0; i < s.triggerEvents[eventName].length; i++) {
-        if(s.triggerEvents[eventName][i] === handler) s.triggerEvents[eventName].splice(i, 1);
+    if (!s.triggerEventListeners[eventName] || s.triggerEventListeners[eventName].length === 0) return;
+    for (i = 0; i < s.triggerEventListeners[eventName].length; i++) {
+        if(s.triggerEventListeners[eventName][i] === handler) s.triggerEventListeners[eventName].splice(i, 1);
     }
     return s;
 };
