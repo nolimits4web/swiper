ea47244f4a02f759327792d20a6572666d63a5b6
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-04-25 21:20:58 +0300

    Add different intro for F7

---
+++ src/js/swiper-intro-f7.js
@@ -0,0 +1,5 @@
+/*===========================
+Swiper
+===========================*/
+window.Swiper = function (container, params) {
+    if (!(this instanceof Swiper)) return new Swiper(container, params);
