977eabe01021ef45c04eabd32233bc67c57907a9
Vladimir Kharlampidi <vladimir.kharlampidi@internavenue.com>
2015-06-14 15:39:37 +0300

    Update dependancies

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 (function(){
     'use strict';
     var gulp = require('gulp'),
-        gutil = require('gulp-util'),
         connect = require('gulp-connect'),
         open = require('gulp-open'),
         less = require('gulp-less'),
