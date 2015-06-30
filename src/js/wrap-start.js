ae83108d493f6b4ba2d6ffdf99dc7c81a5c38c6e
Callum Macrae <callum@macr.ae>
2015-06-30 15:13:44 +0100

    added missing use strict statements

--- src/js/wrap-start.js
+++ src/js/wrap-start.js
@@ -1,4 +1,6 @@
 (function (root, factory) {
+    'use strict';
+
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define([], factory);
