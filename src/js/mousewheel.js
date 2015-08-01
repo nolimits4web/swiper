b7c83d9295a6311023e2f4e18ee4b9d920a669e6
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-08-01 15:44:35 +0300

    Fix issues with loop and mousewheel
    Fixes #1369

--- src/js/mousewheel.js
+++ src/js/mousewheel.js
@@ -65,11 +65,11 @@ function handleMousewheel(e) {
     if (!s.params.freeMode) {
         if ((new window.Date()).getTime() - s.mousewheel.lastScrollTime > 60) {
             if (delta < 0) {
-                if (!s.isEnd) s.slideNext();
+                if (!s.isEnd || s.params.loop) s.slideNext();
                 else if (s.params.mousewheelReleaseOnEdges) return true;
             }
             else {
-                if (!s.isBeginning) s.slidePrev();
+                if (!s.isBeginning || s.params.loop) s.slidePrev();
                 else if (s.params.mousewheelReleaseOnEdges) return true;
             }
         }
