3de5418c752f283dd17473e7eab1b781edd188ba
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-08-22 19:15:39 +0300

    Added support for iOS in-app UIWebView edge-swipe detection
    Fixes #1380

--- src/js/core.js
+++ src/js/core.js
@@ -6,6 +6,9 @@ var defaults = {
     // autoplay
     autoplay: false,
     autoplayDisableOnInteraction: true,
+    // To support iOS's swipe-to-go-back gesture (when being used in-app, with UIWebView).
+    iOSEdgeSwipeDetection: true,
+    iOSEdgeSwipeThreshold: 20,
     // Free mode
     freeMode: false,
     freeModeMomentum: true,
@@ -1154,12 +1157,21 @@ s.onTouchStart = function (e) {
     if (s.params.swipeHandler) {
         if (!findElementInEvent(e, s.params.swipeHandler)) return;
     }
+
+    var startX = s.touches.currentX = e.type === 'touchstart' ? e.targetTouches[0].pageX : e.pageX;
+    var startY = s.touches.currentY = e.type === 'touchstart' ? e.targetTouches[0].pageY : e.pageY;
+    
+    // Do NOT start if iOS edge swipe is detected. Otherwise iOS app (UIWebView) cannot swipe-to-go-back anymore
+    if(s.device.ios && s.params.iOSEdgeSwipeDetection && startX <= s.params.iOSEdgeSwipeThreshold) {
+        return;
+    }
+
     isTouched = true;
     isMoved = false;
     isScrolling = undefined;
     startMoving = undefined;
-    s.touches.startX = s.touches.currentX = e.type === 'touchstart' ? e.targetTouches[0].pageX : e.pageX;
-    s.touches.startY = s.touches.currentY = e.type === 'touchstart' ? e.targetTouches[0].pageY : e.pageY;
+    s.touches.startX = startX;
+    s.touches.startY = startY;
     touchStartTime = Date.now();
     s.allowClick = true;
     s.updateContainerSize();
