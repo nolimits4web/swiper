ea40e0dd074dcf320e25a831cec53adb90079cf3
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-16 01:28:11 +0300

    Add onScroll event
    Fixes #1774

--- src/js/mousewheel.js
+++ src/js/mousewheel.js
@@ -68,11 +68,17 @@ function handleMousewheel(e) {
     if (!s.params.freeMode) {
         if ((new window.Date()).getTime() - s.mousewheel.lastScrollTime > 60) {
             if (delta < 0) {
-                if ((!s.isEnd || s.params.loop) && !s.animating) s.slideNext();
+                if ((!s.isEnd || s.params.loop) && !s.animating) {
+                    s.slideNext();
+                    s.emit('onScroll', s, e);
+                }
                 else if (s.params.mousewheelReleaseOnEdges) return true;
             }
             else {
-                if ((!s.isBeginning || s.params.loop) && !s.animating) s.slidePrev();
+                if ((!s.isBeginning || s.params.loop) && !s.animating) {
+                    s.slidePrev();
+                    s.emit('onScroll', s, e);
+                }
                 else if (s.params.mousewheelReleaseOnEdges) return true;
             }
         }
@@ -108,6 +114,9 @@ function handleMousewheel(e) {
                 s.lazy.load();
             }
         }
+        // Emit event
+        s.emit('onScroll', s, e);
+
         // Stop autoplay
         if (s.params.autoplay && s.params.autoplayDisableOnInteraction) s.stopAutoplay();
 
