c50e585f3a00213904c04be535f69a9fcbe6ee5a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-03-07 12:46:31 +0300

    Add resize callbacks
    Fixes #2037

--- src/js/core.js
+++ src/js/core.js
@@ -197,6 +197,8 @@ var defaults = {
     Callbacks:
     onInit: function (swiper)
     onDestroy: function (swiper)
+    onBeforeResize: function (swiper)
+    onAfterResize: function (swiper)
     onClick: function (swiper, e)
     onTap: function (swiper, e)
     onDoubleTap: function (swiper, e)
@@ -1245,6 +1247,7 @@ s.update = function (updateTranslate) {
   Resize Handler
   ===========================*/
 s.onResize = function (forceUpdatePagination) {
+    if (s.params.onBeforeResize) s.params.onBeforeResize(s);
     //Breakpoints
     if (s.params.breakpoints) {
         s.setBreakpoint();
@@ -1290,6 +1293,7 @@ s.onResize = function (forceUpdatePagination) {
     // Return locks after resize
     s.params.allowSwipeToPrev = allowSwipeToPrev;
     s.params.allowSwipeToNext = allowSwipeToNext;
+    if (s.params.onAfterResize) s.params.onAfterResize(s);
 };
 
 /*=========================
