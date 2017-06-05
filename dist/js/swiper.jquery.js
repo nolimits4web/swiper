22f5510d6cd2b93ebbc210e257b887a96c61f7f8
Andre <almoreira@mongeralaegon.com.br>
2017-06-05 14:09:06 -0300

    add a window load function

--- dist/js/swiper.jquery.js
+++ dist/js/swiper.jquery.js
@@ -4620,20 +4620,23 @@
     /*===========================
      Get Dom libraries
      ===========================*/
-    var swiperDomPlugins = ['jQuery', 'Zepto', 'Dom7'];
-    for (var i = 0; i < swiperDomPlugins.length; i++) {
-    	if (window[swiperDomPlugins[i]]) {
-    		addLibraryPlugin(window[swiperDomPlugins[i]]);
-    	}
-    }
-    // Required DOM Plugins
     var domLib;
-    if (typeof Dom7 === 'undefined') {
-    	domLib = window.Dom7 || window.Zepto || window.jQuery;
-    }
-    else {
-    	domLib = Dom7;
-    }
+    window.onload = function() {
+        var swiperDomPlugins = ['jQuery', 'Zepto', 'Dom7'];
+        for (var i = 0; i < swiperDomPlugins.length; i++) {
+            if (window[swiperDomPlugins[i]]) {
+                addLibraryPlugin(window[swiperDomPlugins[i]]);
+            }
+        }
+        // Required DOM Plugins
+        if (typeof Dom7 === 'undefined') {
+            domLib = window.Dom7 || window.Zepto || window.jQuery;
+        }
+        else {
+            domLib = Dom7;
+        }
+    };
+    
     
 
     /*===========================
