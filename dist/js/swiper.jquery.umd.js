16751f2cd3980a9b40cf05b1f6d9ce3970dab04d
Andre <almoreira@mongeralaegon.com.br>
2017-06-05 13:33:34 -0300

    add a verification to all functions in prototype

--- dist/js/swiper.jquery.umd.js
+++ dist/js/swiper.jquery.umd.js
@@ -4515,7 +4515,11 @@
                 return (ua.indexOf('safari') >= 0 && ua.indexOf('chrome') < 0 && ua.indexOf('android') < 0);
             }
         })(),
-        isUiWebView: /(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(window.navigator.userAgent),
+        isUiWebView: (function () {
+            if (typeof window !== 'undefined') {
+               return /(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(window.navigator.userAgent);
+            }
+        })(),
         isArray: function (arr) {
             return Object.prototype.toString.apply(arr) === '[object Array]';
         },
@@ -4523,16 +4527,22 @@
         Browser
         ====================================================*/
         browser: {
-            ie: window.navigator.pointerEnabled || window.navigator.msPointerEnabled,
+            ie: (function () {
+                if (typeof window !== 'undefined') {
+                   return window.navigator.pointerEnabled || window.navigator.msPointerEnabled;
+                }
+            })(),
             ieTouch: (window.navigator.msPointerEnabled && window.navigator.msMaxTouchPoints > 1) || (window.navigator.pointerEnabled && window.navigator.maxTouchPoints > 1),
-            lteIE9: (function() {
-                // create temporary DIV
-                var div = document.createElement('div');
-                // add content to tmp DIV which is wrapped into the IE HTML conditional statement
-                div.innerHTML = '<!--[if lte IE 9]><i></i><![endif]-->';
-                // return true / false value based on what will browser render
-                return div.getElementsByTagName('i').length === 1;
-            })()
+            lteIE9: (function () {
+                if (typeof document !== 'undefined') {
+                    // create temporary DIV
+                    var div = document.createElement('div');
+                    // add content to tmp DIV which is wrapped into the IE HTML conditional statement
+                    div.innerHTML = '<!--[if lte IE 9]><i></i><![endif]-->';
+                    // return true / false value based on what will browser render
+                    return div.getElementsByTagName('i').length === 1;
+                }
+            })(),
         },
         /*==================================================
         Devices
@@ -4554,42 +4564,54 @@
         Feature Detection
         ====================================================*/
         support: {
-            touch : (window.Modernizr && Modernizr.touch === true) || (function () {
-                return !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+            touch: (window.Modernizr && Modernizr.touch === true) || (function () {
+                if (typeof window !== 'undefined') {
+                    return !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+                }
             })(),
     
-            transforms3d : (window.Modernizr && Modernizr.csstransforms3d === true) || (function () {
-                var div = document.createElement('div').style;
-                return ('webkitPerspective' in div || 'MozPerspective' in div || 'OPerspective' in div || 'MsPerspective' in div || 'perspective' in div);
+            transforms3d: (window.Modernizr && Modernizr.csstransforms3d === true) || (function () {
+                if (typeof document !== 'undefined') {
+                    var div = document.createElement('div').style;
+                    return ('webkitPerspective' in div || 'MozPerspective' in div || 'OPerspective' in div || 'MsPerspective' in div || 'perspective' in div);
+                }
             })(),
     
             flexbox: (function () {
-                var div = document.createElement('div').style;
-                var styles = ('alignItems webkitAlignItems webkitBoxAlign msFlexAlign mozBoxAlign webkitFlexDirection msFlexDirection mozBoxDirection mozBoxOrient webkitBoxDirection webkitBoxOrient').split(' ');
-                for (var i = 0; i < styles.length; i++) {
-                    if (styles[i] in div) return true;
+                if (typeof document !== 'undefined') {
+                    var div = document.createElement('div').style;
+                    var styles = ('alignItems webkitAlignItems webkitBoxAlign msFlexAlign mozBoxAlign webkitFlexDirection msFlexDirection mozBoxDirection mozBoxOrient webkitBoxDirection webkitBoxOrient').split(' ');
+                    for (var i = 0; i < styles.length; i++) {
+                        if (styles[i] in div) return true;
+                    }
                 }
             })(),
     
             observer: (function () {
-                return ('MutationObserver' in window || 'WebkitMutationObserver' in window);
+                if (typeof window !== 'undefined') {
+                    return ('MutationObserver' in window || 'WebkitMutationObserver' in window);
+                }
             })(),
     
             passiveListener: (function () {
-                var supportsPassive = false;
-                try {
-                    var opts = Object.defineProperty({}, 'passive', {
-                        get: function() {
-                            supportsPassive = true;
-                        }
-                    });
-                    window.addEventListener('testPassiveListener', null, opts);
-                } catch (e) {}
-                return supportsPassive;
+                if (typeof window !== 'undefined') {
+                    var supportsPassive = false;
+                    try {
+                        var opts = Object.defineProperty({}, 'passive', {
+                            get: function() {
+                                supportsPassive = true;
+                            }
+                        });
+                        window.addEventListener('testPassiveListener', null, opts);
+                    } catch (e) {}
+                    return supportsPassive;
+                }
             })(),
     
             gestures: (function () {
-                return 'ongesturestart' in window;
+                if (typeof window !== 'undefined') {
+                    return 'ongesturestart' in window;
+                }
             })()
         },
         /*==================================================
