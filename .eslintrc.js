4f573d95aaf9476962a0156585d8aba5973695b9
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-23 00:44:58 +0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
 module.exports = {
   "extends": "airbnb-base",
   "plugins": [
-  "import"
+    "import"
   ],
+  "rules": {
+    "max-len": ["error", 1000, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
+  },
   "globals": {
     "window": true,
     "document": true,
     "Modernizr": true,
-    "DocumentTouch": true
+    "DocumentTouch": true,
   }
 };
