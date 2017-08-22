2bd379ed4d8070349852e78b1520b78686db5800
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-23 00:47:31 +0300

    Navigation component

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,144 @@
+import $ from '../../utils/dom';
+import Utils from '../../utils/utils';
+
+const Navigation = {
+  update() {
+    // Update Navigation Buttons
+    const swiper = this;
+    const params = swiper.params.navigation;
+
+    if (swiper.params.loop) return;
+    const { $nextEl, $prevEl } = swiper.navigation;
+
+    if ($prevEl && $prevEl.length > 0) {
+      if (swiper.isBeginning) {
+        $prevEl.addClass(params.disabledClass);
+        // if (s.params.a11y && s.a11y) s.a11y.disable(s.prevButton);
+      } else {
+        $prevEl.removeClass(params.disabledClass);
+        // if (s.params.a11y && s.a11y) s.a11y.enable(s.prevButton);
+      }
+    }
+    if ($nextEl && $nextEl.length > 0) {
+      if (swiper.isEnd) {
+        $nextEl.addClass(params.disabledClass);
+        // if (s.params.a11y && s.a11y) s.a11y.disable(s.nextButton);
+      } else {
+        $nextEl.removeClass(params.disabledClass);
+        // if (s.params.a11y && s.a11y) s.a11y.enable(s.nextButton);
+      }
+    }
+  },
+  init() {
+    const swiper = this;
+    const params = swiper.params.navigation;
+    if (!(params.nextEl || params.prevEl)) return;
+
+    let $nextEl;
+    let $prevEl;
+    if (params.nextEl) {
+      $nextEl = $(params.nextEl);
+      if (
+        swiper.params.uniqueNavElements &&
+          typeof params.nextEl === 'string' &&
+          swiper.navigation.$nextEl.length > 1 &&
+          swiper.$el.find(params.nextEl).length === 1
+      ) {
+        swiper.navigation.$nextEl = swiper.$el.find(params.nextEl);
+      }
+    }
+    if (params.prevEl) {
+      $prevEl = $(params.prevEl);
+      if (
+        swiper.params.uniqueNavElements &&
+          typeof params.prevEl === 'string' &&
+          swiper.navigation.$prevEl.length > 1 &&
+          swiper.$el.find(params.prevEl).length === 1
+      ) {
+        swiper.navigation.$prevEl = swiper.$el.find(params.prevEl);
+      }
+    }
+
+    if (!($nextEl || $prevEl) || ($nextEl.length === 0 && $prevEl.length === 0)) return;
+
+    if ($nextEl && $nextEl.length > 0) {
+      $nextEl.on('click', (e) => {
+        e.preventDefault();
+        if (swiper.isEnd && !swiper.params.loop) return;
+        swiper.slideNext();
+      });
+      // if (s.params.a11y && s.a11y) s.nextButton[actionDom]('keydown', s.a11y.onEnterKey);
+    }
+    if ($prevEl && $prevEl.length > 0) {
+      $prevEl.on('click', (e) => {
+        e.preventDefault();
+        if (swiper.isBeginning && !swiper.params.loop) return;
+        swiper.slidePrev();
+      });
+      // if (s.params.a11y && s.a11y) s.prevButton[actionDom]('keydown', s.a11y.onEnterKey);
+    }
+
+    Utils.extend(swiper.navigation, {
+      $nextEl,
+      nextEl: $nextEl[0],
+      $prevEl,
+      prevEl: $prevEl[0],
+    });
+  },
+  destroy() {
+    const swiper = this;
+    const { $nextEl, $prevEl } = swiper.navigation;
+    if ($nextEl && $nextEl.length) {
+      $nextEl.off('click');
+    }
+    if ($prevEl && $prevEl.length) {
+      $prevEl.off('click');
+    }
+  },
+};
+
+export default {
+  name: 'navgiation',
+  params: {
+    navigation: {
+      nextEl: null,
+      prevEl: null,
+
+      hideOnClick: false,
+      disabledClass: 'swiper-button-disabled',
+      hiddenClass: 'swiper-button-hidden',
+    },
+  },
+  create() {
+    const swiper = this;
+    Utils.extend(swiper, {
+      navigation: {
+        init: Navigation.init.bind(swiper),
+        update: Navigation.update.bind(swiper),
+      },
+    });
+  },
+  on: {
+    init() {
+      const swiper = this;
+      swiper.navigation.init();
+    },
+    destroy() {
+      const swiper = this;
+      swiper.navigation.destroy();
+    },
+    click(e) {
+      const swiper = this;
+      const { $nextEl, $prevEl } = swiper.navigation;
+      if (!($nextEl || $prevEl) || ($nextEl.length === 0 && $prevEl.length === 0)) return;
+      if (
+        swiper.params.navigation.hideOnClick &&
+        !$(e.target).is($prevEl) &&
+        !$(e.target).is($nextEl)
+      ) {
+        if ($nextEl) $nextEl.toggleClass(swiper.params.navigation.hiddenClass);
+        if ($prevEl) $prevEl.toggleClass(swiper.params.navigation.hiddenClass);
+      }
+    },
+  },
+};
