5103516503df1511bc8eba4fab280595944aa0fa
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-23 16:40:27 +0300

    Loop methods

---
+++ src/components/core/loop/index.js
@@ -0,0 +1,9 @@
+import loopCreate from './loopCreate';
+import loopFix from './loopFix';
+import loopDestroy from './loopDestroy';
+
+export {
+  loopCreate,
+  loopFix,
+  loopDestroy,
+};
---
+++ src/components/core/loop/loopDestroy.js
@@ -0,0 +1,6 @@
+export default function () {
+  const swiper = this;
+  const { $wrapperEl, params, slides } = swiper;
+  $wrapperEl.children(`.${params.slideClass}.${params.slideDuplicateClass}`).remove();
+  slides.removeAttr('data-swiper-slide-index');
+}
