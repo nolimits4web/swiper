1b421e4b23db7fc869156d7d35b7ea68536d02ca
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-23 16:40:40 +0300

    Fix active index + new loop methods

--- src/components/core/slide/slideNext.js
+++ src/components/core/slide/slideNext.js
@@ -1,12 +1,12 @@
 /* eslint no-unused-vars: "off" */
 export default function (speed = this.params.speed, runCallbacks = true, internal) {
   const swiper = this;
-  const { params, animating, activeIndex } = swiper;
+  const { params, animating } = swiper;
   if (params.loop) {
     if (animating) return false;
-    swiper.loop.fix();
+    swiper.loopFix();
     const clientLeft = swiper.$wrapperEl[0].clientLeft;
-    return swiper.slideTo(activeIndex + params.slidesPerGroup, speed, runCallbacks, internal);
+    return swiper.slideTo(swiper.activeIndex + params.slidesPerGroup, speed, runCallbacks, internal);
   }
-  return swiper.slideTo(activeIndex + params.slidesPerGroup, speed, runCallbacks, internal);
+  return swiper.slideTo(swiper.activeIndex + params.slidesPerGroup, speed, runCallbacks, internal);
 }
--- src/components/core/slide/slidePrev.js
+++ src/components/core/slide/slidePrev.js
@@ -1,13 +1,13 @@
 /* eslint no-unused-vars: "off" */
 export default function (speed = this.params.speed, runCallbacks = true, internal) {
   const swiper = this;
-  const { params, animating, activeIndex } = swiper;
+  const { params, animating } = swiper;
 
   if (params.loop) {
     if (animating) return false;
-    swiper.loop.fix();
+    swiper.loopFix();
     const clientLeft = swiper.$wrapperEl[0].clientLeft;
-    return swiper.slideTo(activeIndex - 1, speed, runCallbacks, internal);
+    return swiper.slideTo(swiper.activeIndex - 1, speed, runCallbacks, internal);
   }
-  return swiper.slideTo(activeIndex - 1, speed, runCallbacks, internal);
+  return swiper.slideTo(swiper.activeIndex - 1, speed, runCallbacks, internal);
 }
