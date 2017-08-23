d022a3f517c9a738c7b172ca9da6bb79fd31fe20
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-23 18:36:27 +0300

    Slides manipulations

---
+++ src/components/core/slides-manipulation/prependSlide.js
@@ -0,0 +1,26 @@
+import Support from '../../../utils/support';
+
+export default function (slides) {
+  const swiper = this;
+  const { params, $wrapperEl, activeIndex } = swiper;
+
+  if (params.loop) {
+    swiper.loopDestroy();
+  }
+  let newActiveIndex = activeIndex + 1;
+  if (typeof slides === 'object' && slides.length) {
+    for (let i = 0; i < slides.length; i += 1) {
+      if (slides[i]) $wrapperEl.prepend(slides[i]);
+    }
+    newActiveIndex = activeIndex + slides.length;
+  } else {
+    $wrapperEl.prepend(slides);
+  }
+  if (params.loop) {
+    swiper.loopCreate();
+  }
+  if (!(params.observer && Support.observer)) {
+    swiper.update(true);
+  }
+  swiper.slideTo(newActiveIndex, 0, false);
+}
---
+++ src/components/core/slides-manipulation/removeAllSlides.js
@@ -0,0 +1,9 @@
+export default function () {
+  const swiper = this;
+
+  const slidesIndexes = [];
+  for (let i = 0; i < swiper.slides.length; i += 1) {
+    slidesIndexes.push(i);
+  }
+  swiper.removeSlide(slidesIndexes);
+}
