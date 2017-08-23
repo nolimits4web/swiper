de51025e4adebe7b76551bae91bb0147b9b38cd9
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-23 16:40:48 +0300

    Reloop

--- src/components/core/setBreakpoint.js
+++ src/components/core/setBreakpoint.js
@@ -22,19 +22,25 @@ function getBreakpoint(breakpoints) {
 
 export default function () {
   const swiper = this;
-  const breakpoints = swiper.params.breakpoints;
+  const { activeIndex, loopedSlides, params } = swiper;
+  const breakpoints = params.breakpoints;
   if (!breakpoints || (breakpoints && Object.keys(breakpoints).length === 0)) return;
   // Set breakpoint for window width and update parameters
   const breakpoint = getBreakpoint(breakpoints);
   if (breakpoint && currentBreakpoint !== breakpoint) {
     const breakPointsParams = breakpoint in breakpoints ? breakpoints[breakpoint] : swiper.originalParams;
-    const needsReLoop = swiper.params.loop && (breakPointsParams.slidesPerView !== swiper.params.slidesPerView);
+    const needsReLoop = params.loop && (breakPointsParams.slidesPerView !== params.slidesPerView);
 
     Utils.extend(swiper.params, breakPointsParams);
 
     currentBreakpoint = breakpoint;
-    // if (needsReLoop && swiper.destroyLoop) {
-    //   swiper.reLoop(true);
-    // }
+
+    if (needsReLoop) {
+      const oldIndex = activeIndex - loopedSlides;
+      swiper.loopDestroy();
+      swiper.loopCreate();
+      swiper.updateSlides();
+      swiper.slideTo(oldIndex + loopedSlides, 0, false);
+    }
   }
 }
