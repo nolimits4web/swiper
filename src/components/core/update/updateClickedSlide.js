4d526f226b4a4eed19672c75bfbf8312a2aa1857
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-24 13:33:33 +0300

    update clicked slide

---
+++ src/components/core/update/updateClickedSlide.js
@@ -0,0 +1,25 @@
+import $ from '../../../utils/dom';
+
+export default function (e) {
+  const swiper = this;
+  const params = swiper.params;
+  const slide = $(e.target).closest(`.${params.slideClass}`)[0];
+  let slideFound = false;
+  if (slide) {
+    for (let i = 0; i < swiper.slides.length; i += 1) {
+      if (swiper.slides[i] === slide) slideFound = true;
+    }
+  }
+
+  if (slide && slideFound) {
+    swiper.clickedSlide = slide;
+    swiper.clickedIndex = $(slide).index();
+  } else {
+    swiper.clickedSlide = undefined;
+    swiper.clickedIndex = undefined;
+    return;
+  }
+  if (params.slideToClickedSlide && swiper.clickedIndex !== undefined && swiper.clickedIndex !== swiper.activeIndex) {
+    swiper.slideToClickedSlide();
+  }
+}
