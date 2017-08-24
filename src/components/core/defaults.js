d00a836ffb8e68fa76908ed5b8ee9884b5d06acd
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-24 15:17:09 +0300

    Remove unused vars

--- src/components/core/defaults.js
+++ src/components/core/defaults.js
@@ -60,14 +60,6 @@ export default {
   scrollbarHide: true,
   scrollbarDraggable: false,
   scrollbarSnapOnRelease: false,
-  // Keyboard Mousewheel
-  // keyboardControl: false,
-  // mousewheelControl: false,
-  // mousewheelReleaseOnEdges: false,
-  // mousewheelInvert: false,
-  // mousewheelForceToAxis: false,
-  // mousewheelSensitivity: 1,
-  // mousewheelEventsTarged: 'container',
   // Hash Navigation
   hashnav: false,
   hashnavWatchState: false,
@@ -103,22 +95,9 @@ export default {
   touchReleaseOnEdges: false,
   // Unique Navigation Elements
   uniqueNavElements: true,
-  // Pagination
-  // pagination: null,
-  // paginationElement: 'span',
-  // paginationClickable: false,
-  // paginationHide: false,
-  // paginationBulletRender: null,
-  // paginationProgressRender: null,
-  // paginationFractionRender: null,
-  // paginationCustomRender: null,
-  // paginationType: 'bullets', // 'bullets' or 'progress' or 'fraction' or 'custom'
   // Resistance
   resistance: true,
   resistanceRatio: 0.85,
-  // Next/prev buttons
-  // nextButton: null,
-  // prevButton: null,
   // Progress
   watchSlidesProgress: false,
   watchSlidesVisibility: false,
@@ -167,15 +146,7 @@ export default {
   slidePrevClass: 'swiper-slide-prev',
   slideDuplicatePrevClass: 'swiper-slide-duplicate-prev',
   wrapperClass: 'swiper-wrapper',
-  // bulletClass: 'swiper-pagination-bullet',
-  // bulletActiveClass: 'swiper-pagination-bullet-active',
-  // buttonDisabledClass: 'swiper-button-disabled',
-  // paginationCurrentClass: 'swiper-pagination-current',
-  // paginationTotalClass: 'swiper-pagination-total',
-  // paginationHiddenClass: 'swiper-pagination-hidden',
-  // paginationProgressbarClass: 'swiper-pagination-progressbar',
-  // paginationClickableClass: 'swiper-pagination-clickable', // NEW
-  // paginationModifierClass: 'swiper-pagination-', // NEW
+
   lazyLoadingClass: 'swiper-lazy',
   lazyStatusLoadingClass: 'swiper-lazy-loading',
   lazyStatusLoadedClass: 'swiper-lazy-loaded',
@@ -187,6 +158,7 @@ export default {
   // Observer
   observer: false,
   observeParents: false,
+
   // Accessibility
   a11y: false,
   prevSlideMessage: 'Previous slide',
@@ -196,34 +168,4 @@ export default {
   paginationBulletMessage: 'Go to slide {{index}}',
   // Callbacks
   runCallbacksOnInit: true,
-  /*
-  Callbacks:
-  onInit: function (swiper)
-  onDestroy: function (swiper)
-  onBeforeResize: function (swiper)
-  onAfterResize: function (swiper)
-  onClick: function (swiper, e)
-  onTap: function (swiper, e)
-  onDoubleTap: function (swiper, e)
-  onSliderMove: function (swiper, e)
-  onSlideChangeStart: function (swiper)
-  onSlideChangeEnd: function (swiper)
-  onTransitionStart: function (swiper)
-  onTransitionEnd: function (swiper)
-  onImagesReady: function (swiper)
-  onProgress: function (swiper, progress)
-  onTouchStart: function (swiper, e)
-  onTouchMove: function (swiper, e)
-  onTouchMoveOpposite: function (swiper, e)
-  onTouchEnd: function (swiper, e)
-  onReachBeginning: function (swiper)
-  onReachEnd: function (swiper)
-  onSetTransition: function (swiper, duration)
-  onSetTranslate: function (swiper, translate)
-  onAutoplayStart: function (swiper)
-  onAutoplayStop: function (swiper),
-  onLazyImageLoad: function (swiper, slide, image)
-  onLazyImageReady: function (swiper, slide, image)
-  onKeyPress: function (swiper, keyCode)
-  */
 };
