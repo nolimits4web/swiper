906008536ce7a939a9934c5e5fc1a5508904b25f
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-24 13:44:42 +0300

    Fix rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,28 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "plugins": [
-    "import"
+  extends: 'airbnb-base',
+  plugins: [
+    'import'
   ],
-  "rules": {
-    "max-len": ["error", 1000, 2, {
+  rules: {
+    'max-len': ['error', 1000, 2, {
       ignoreUrls: true,
       ignoreComments: false,
       ignoreRegExpLiterals: true,
       ignoreStrings: true,
       ignoreTemplateLiterals: true,
     }],
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
   },
-  "globals": {
-    "window": true,
-    "document": true,
-    "Modernizr": true,
-    "DocumentTouch": true,
+  globals: {
+    'window': true,
+    'document': true,
+    'Modernizr': true,
+    'DocumentTouch': true,
   }
 };
