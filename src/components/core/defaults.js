7942bd7861d55f5e84b6809cd1d24540cc928877
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-29 15:39:02 +0300

    Remove unused params

--- src/components/core/defaults.js
+++ src/components/core/defaults.js
@@ -4,9 +4,11 @@ export default {
   touchEventsTarget: 'container',
   initialSlide: 0,
   speed: 300,
+
   // To support iOS's swipe-to-go-back gesture (when being used in-app, with UIWebView).
   iOSEdgeSwipeDetection: false,
   iOSEdgeSwipeThreshold: 20,
+
   // Free mode
   freeMode: false,
   freeModeMomentum: true,
@@ -16,36 +18,22 @@ export default {
   freeModeMomentumVelocityRatio: 1,
   freeModeSticky: false,
   freeModeMinimumVelocity: 0.02,
+
   // Autoheight
   autoHeight: false,
+
   // Set wrapper width
   setWrapperSize: false,
+
   // Virtual Translate
   virtualTranslate: false,
+
   // Effects
   effect: 'slide', // 'slide' or 'fade' or 'cube' or 'coverflow' or 'flip'
-  coverflow: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  flip: {
-    slideShadows: true,
-    limitRotation: true,
-  },
-  cube: {
-    slideShadows: true,
-    shadow: true,
-    shadowOffset: 20,
-    shadowScale: 0.94,
-  },
-  fade: {
-    crossFade: false,
-  },
+
   // Breakpoints
   breakpoints: undefined,
+
   // Slides grid
   spaceBetween: 0,
   slidesPerView: 1,
@@ -55,8 +43,11 @@ export default {
   centeredSlides: false,
   slidesOffsetBefore: 0, // in px
   slidesOffsetAfter: 0, // in px
+  normalizeSlideIndex: true,
+
   // Round length
   roundLengths: false,
+
   // Touches
   touchRatio: 1,
   touchAngle: 45,
@@ -70,41 +61,46 @@ export default {
   threshold: 0,
   touchMoveStopPropagation: true,
   touchReleaseOnEdges: false,
+
   // Unique Navigation Elements
   uniqueNavElements: true,
+
   // Resistance
   resistance: true,
   resistanceRatio: 0.85,
+
   // Progress
   watchSlidesProgress: false,
   watchSlidesVisibility: false,
+
   // Cursor
   grabCursor: false,
+
   // Clicks
   preventClicks: true,
   preventClicksPropagation: true,
   slideToClickedSlide: false,
+
   // Images
   preloadImages: true,
   updateOnImagesReady: true,
+
   // loop
   loop: false,
   loopAdditionalSlides: 0,
   loopedSlides: null,
   loopFillGroupWithBlank: false,
-  // Control
-  control: undefined,
-  controlInverse: false,
-  controlBy: 'slide', // or 'container'
-  normalizeSlideIndex: true,
+
   // Swiping/no swiping
   allowSlidePrev: true,
   allowSlideNext: true,
   swipeHandler: null, // '.swipe-handler',
   noSwiping: true,
   noSwipingClass: 'swiper-no-swiping',
+
   // Passive Listeners
   passiveListeners: true,
+
   // NS
   containerModifierClass: 'swiper-container-', // NEW
   slideClass: 'swiper-slide',
@@ -134,6 +130,7 @@ export default {
   firstSlideMessage: 'This is the first slide',
   lastSlideMessage: 'This is the last slide',
   paginationBulletMessage: 'Go to slide {{index}}',
+
   // Callbacks
   runCallbacksOnInit: true,
 };
