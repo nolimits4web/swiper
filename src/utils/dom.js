209055892a52ec53ac421945e1f509a9d9f93ead
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-30 13:47:58 +0300

    Custom dom7

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,3 +1,10 @@
-import $ from 'dom7';
+import $ from 'dom7/src/$';
+import Methods from 'dom7/src/methods';
+
+// Methods
+Object.keys(Methods).forEach((key) => {
+  if ($.fn[key]) return;
+  $.fn[key] = Methods[key];
+});
 
 export default $;
