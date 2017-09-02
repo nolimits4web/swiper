eca8a5ce4be313866b48b4004c864dcf26c3a1b9
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-02 15:15:34 +0300

    Update to new Dom7

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -2,9 +2,6 @@ import $ from 'dom7/src/$';
 import Methods from 'dom7/src/methods';
 
 // Methods
-Object.keys(Methods).forEach((key) => {
-  if ($.fn[key]) return;
-  $.fn[key] = Methods[key];
-});
+$.use(Methods);
 
 export default $;
