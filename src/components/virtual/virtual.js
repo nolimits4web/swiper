f51173ffdaa54acf2c9796f1441094a3157d5392
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-10-17 13:30:58 +0300

    Add slide index if not presented

--- src/components/virtual/virtual.js
+++ src/components/virtual/virtual.js
@@ -111,7 +111,7 @@ const Virtual = {
     const $slideEl = params.renderSlide
       ? $(params.renderSlide.call(swiper, slide, index))
       : $(`<div class="${swiper.params.slideClass}" data-swiper-slide-index="${index}">${slide}</div>`);
-
+    if (!$slideEl.attr('data-swiper-slide-index')) $slideEl.attr('data-swiper-slide-index', index);
     if (params.cache) swiper.virtual.cache[index] = $slideEl;
     return $slideEl;
   },
