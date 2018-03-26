23221b9ff12992fcc06215af83b1c8338ffac158
fenyagg <fenyagg@gmail.com>
2018-03-26 17:13:20 +0300

    checkOverflow: click problem && add lock events
    some click events did not work with checkOverflow=true

--- src/components/core/check-overflow/index.js
+++ src/components/core/check-overflow/index.js
@@ -4,7 +4,11 @@ function checkOverflow() {
   const wasLocked = swiper.isLocked;
 
   swiper.isLocked = swiper.snapGrid.length === 1;
-  swiper.allowTouchMove = !swiper.isLocked;
+  swiper.allowSlideNext = !swiper.isLocked;
+  swiper.allowSlidePrev = !swiper.isLocked;
+
+  // events
+  if (wasLocked !== swiper.isLocked) swiper.emit(swiper.isLocked ? 'lock' : 'unlock');
 
   if (wasLocked && wasLocked !== swiper.isLocked) {
     swiper.isEnd = false;
