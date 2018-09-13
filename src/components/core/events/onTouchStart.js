6989049120b2591f50b0f4bf0dbece5cf21d545f
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-09-13 17:55:09 +0300

    Don't track all mouse buttons
    Fixes #2784

--- src/components/core/events/onTouchStart.js
+++ src/components/core/events/onTouchStart.js
@@ -13,6 +13,7 @@ export default function (event) {
   if (e.originalEvent) e = e.originalEvent;
   data.isTouchEvent = e.type === 'touchstart';
   if (!data.isTouchEvent && 'which' in e && e.which === 3) return;
+  if (!data.isTouchEvent && 'button' in e && e.button > 0) return;
   if (data.isTouched && data.isMoved) return;
   if (params.noSwiping && $(e.target).closest(params.noSwipingSelector ? params.noSwipingSelector : `.${params.noSwipingClass}`)[0]) {
     swiper.allowClick = true;
