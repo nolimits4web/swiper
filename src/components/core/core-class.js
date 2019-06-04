a1382bec7a3553f1d8a898d5e9d03b8bd4365984
weiz18 <wzhou@yoobic.com>
2019-06-04 11:51:53 +0100

    fix return

--- src/components/core/core-class.js
+++ src/components/core/core-class.js
@@ -125,7 +125,7 @@ class Swiper extends SwiperClass {
     if (el && el.shadowRoot && el.shadowRoot.children) {
       $wrapperEl = $(el.shadowRoot.children[1]);
       // Children needs to return slot items
-      $wrapperEl.children = (options) => { $el.children(options); };
+      $wrapperEl.children = options => $el.children(options);
     } else {
       $wrapperEl = $el.children(`.${swiper.params.wrapperClass}`);
     }
