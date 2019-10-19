a03bc8270027e6f51ea21faea8406f48d3db65c3
Robert F. Popeleski <therfp97@gmail.com>
2019-10-19 17:37:34 -0400

    Lint fixes

--- src/components/core/check-overflow/index.js
+++ src/components/core/check-overflow/index.js
@@ -3,7 +3,7 @@ function checkOverflow() {
   const swiper = this;
   const params = swiper.params;
   const wasLocked = swiper.isLocked;
-  let lastSlidePosition = (params.slidesOffsetBefore + (params.spaceBetween * (swiper.slides.length - 1)) + ((swiper.slides[0]).offsetWidth) * swiper.slides.length)
+  const lastSlidePosition = (params.slidesOffsetBefore + (params.spaceBetween * (swiper.slides.length - 1)) + ((swiper.slides[0]).offsetWidth) * swiper.slides.length);
 
   if ((typeof params.slidesOffsetBefore !== 'undefined' && params.slidesOffsetBefore !== 0) || (typeof params.slidesOffsetAfter !== 'undefined' && params.slidesOffsetAfter !== 0)) {
     swiper.isLocked = lastSlidePosition <= swiper.size;
