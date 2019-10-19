79a6f91582586ba484b493ce56fef1743fad2a09
Robert F. Popeleski <therfp97@gmail.com>
2019-10-19 17:27:01 -0400

    Modified checkOverflow() to account for slide offsets

--- src/components/core/check-overflow/index.js
+++ src/components/core/check-overflow/index.js
@@ -1,9 +1,16 @@
 
 function checkOverflow() {
   const swiper = this;
+  const params = swiper.params;
   const wasLocked = swiper.isLocked;
+  let lastSlidePosition = (params.slidesOffsetBefore + (params.spaceBetween * (swiper.slides.length - 1)) + ((swiper.slides[0]).offsetWidth) * swiper.slides.length)
 
-  swiper.isLocked = swiper.snapGrid.length === 1;
+  if ((typeof params.slidesOffsetBefore !== 'undefined' && params.slidesOffsetBefore !== 0) || (typeof params.slidesOffsetAfter !== 'undefined' && params.slidesOffsetAfter !== 0)) {
+    swiper.isLocked = lastSlidePosition <= swiper.size;
+  } else {
+    swiper.isLocked = swiper.snapGrid.length === 1;
+  }
+
   swiper.allowSlideNext = !swiper.isLocked;
   swiper.allowSlidePrev = !swiper.isLocked;
 
