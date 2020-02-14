08329eddd150f1f06d3ee518558a36b340f7f6d6
Tim Paul <tim.paul@immutable.com>
2020-02-14 14:50:24 +1100

    slim-down and add bits of documentation to functionality + fresh built assets

--- package/js/swiper.esm.js
+++ package/js/swiper.esm.js
@@ -713,21 +713,10 @@ function updateSlides () {
 
 function updateAutoHeight (speed) {
   const swiper = this;
-  let newHeight = 0;
-  let i;
-  if (typeof speed === 'number') {
-    swiper.setTransition(speed);
-  } else if (speed === true) {
-    swiper.setTransition(swiper.params.speed);
-  }
 
-  // Find slides currently in view
-  let activeSlides = [];
-  if (
-    swiper.params.slidesPerView !== 'auto'
-    && swiper.params.slidesPerView > 1
-    && swiper.params.centeredSlides
-  ) {
+  // Helper function to get all active slides when the layout is centered:
+  const getActiveSlidesWhenCentered = () => {
+    const activeSlides = [];
     let centerSlideIndex = 0;
     const slidesArray = Array.from(swiper.slides);
 
@@ -738,39 +727,62 @@ function updateAutoHeight (speed) {
         centerSlideIndex = index;
       }
     });
-    let amountOfSlidesOnEachSide = Math.floor(Math.ceil(swiper.params.slidesPerView) / 2);
+    let amountOfSlidesOnEachSide = Math.floor(
+      Math.ceil(swiper.params.slidesPerView) / 2
+    );
 
-    // Then get each slide outwards - that is currently in view ...
+    // Then get each slide inwards from the bouncds of the view:
     while (amountOfSlidesOnEachSide > 0) {
-      const nextSlide = slidesArray[centerSlideIndex + amountOfSlidesOnEachSide];
+      const nextSlide = slidesArray[
+        centerSlideIndex + amountOfSlidesOnEachSide
+      ];
       if (nextSlide) activeSlides.push(nextSlide);
-      const prevSlide = slidesArray[centerSlideIndex - amountOfSlidesOnEachSide];
+      const prevSlide = slidesArray[
+        centerSlideIndex - amountOfSlidesOnEachSide
+      ];
       if (prevSlide) activeSlides.push(prevSlide);
       amountOfSlidesOnEachSide -= 1;
     }
-  } else if (
-    swiper.params.slidesPerView !== 'auto'
-    && swiper.params.slidesPerView > 1
-  ) {
-    for (i = 0; i < Math.ceil(swiper.params.slidesPerView); i += 1) {
+
+    return activeSlides;
+  };
+
+  // Helper function to get all active slides:
+  const getActiveSlides = () => {
+    const activeSlides = [];
+    for (let i = 0; i < Math.ceil(swiper.params.slidesPerView); i += 1) {
       const index = swiper.activeIndex + i;
       if (index > swiper.slides.length) break;
       activeSlides.push(swiper.slides[index]);
     }
-  } else {
-    activeSlides.push(swiper.slides.eq(swiper.activeIndex)[0]);
+  };
+
+  if (typeof speed === 'number') {
+    swiper.setTransition(speed);
+  } else if (speed === true) {
+    swiper.setTransition(swiper.params.speed);
   }
 
-  // Find new height from highest slide in view
-  for (i = 0; i < activeSlides.length; i += 1) {
-    if (typeof activeSlides[i] !== 'undefined') {
-      const height = activeSlides[i].offsetHeight;
-      newHeight = height > newHeight ? height : newHeight;
-    }
+  // Find slides currently in view
+  let activeSlides = [swiper.slides.eq(swiper.activeIndex)[0]];
+  if (
+    swiper.params.slidesPerView !== 'auto'
+    && swiper.params.slidesPerView > 1
+  ) {
+    activeSlides = swiper.params.centeredSlides
+      ? getActiveSlidesWhenCentered()
+      : getActiveSlides();
   }
+
+  // Find new max height from highest slide in view
+  const maxHeight = activeSlides.reduce((accum, slide) => {
+    return accum < slide.offsetHeight
+      ? slide.offsetHeight
+      : accum;
+  }, 0);
 
   // Update Height
-  if (newHeight) swiper.$wrapperEl.css('height', `${newHeight}px`);
+  if (maxHeight) swiper.$wrapperEl.css('height', `${maxHeight}px`);
 }
 
 function updateSlidesOffset () {
--- package/js/swiper.js
+++ package/js/swiper.js
@@ -1539,21 +1539,10 @@
 
   function updateAutoHeight (speed) {
     var swiper = this;
-    var newHeight = 0;
-    var i;
-    if (typeof speed === 'number') {
-      swiper.setTransition(speed);
-    } else if (speed === true) {
-      swiper.setTransition(swiper.params.speed);
-    }
 
-    // Find slides currently in view
-    var activeSlides = [];
-    if (
-      swiper.params.slidesPerView !== 'auto'
-      && swiper.params.slidesPerView > 1
-      && swiper.params.centeredSlides
-    ) {
+    // Helper function to get all active slides when the layout is centered:
+    var getActiveSlidesWhenCentered = function () {
+      var activeSlides = [];
       var centerSlideIndex = 0;
       var slidesArray = Array.from(swiper.slides);
 
@@ -1564,39 +1553,62 @@
           centerSlideIndex = index;
         }
       });
-      var amountOfSlidesOnEachSide = Math.floor(Math.ceil(swiper.params.slidesPerView) / 2);
+      var amountOfSlidesOnEachSide = Math.floor(
+        Math.ceil(swiper.params.slidesPerView) / 2
+      );
 
-      // Then get each slide outwards - that is currently in view ...
+      // Then get each slide inwards from the bouncds of the view:
       while (amountOfSlidesOnEachSide > 0) {
-        var nextSlide = slidesArray[centerSlideIndex + amountOfSlidesOnEachSide];
+        var nextSlide = slidesArray[
+          centerSlideIndex + amountOfSlidesOnEachSide
+        ];
         if (nextSlide) { activeSlides.push(nextSlide); }
-        var prevSlide = slidesArray[centerSlideIndex - amountOfSlidesOnEachSide];
+        var prevSlide = slidesArray[
+          centerSlideIndex - amountOfSlidesOnEachSide
+        ];
         if (prevSlide) { activeSlides.push(prevSlide); }
         amountOfSlidesOnEachSide -= 1;
       }
-    } else if (
-      swiper.params.slidesPerView !== 'auto'
-      && swiper.params.slidesPerView > 1
-    ) {
-      for (i = 0; i < Math.ceil(swiper.params.slidesPerView); i += 1) {
+
+      return activeSlides;
+    };
+
+    // Helper function to get all active slides:
+    var getActiveSlides = function () {
+      var activeSlides = [];
+      for (var i = 0; i < Math.ceil(swiper.params.slidesPerView); i += 1) {
         var index = swiper.activeIndex + i;
         if (index > swiper.slides.length) { break; }
         activeSlides.push(swiper.slides[index]);
       }
-    } else {
-      activeSlides.push(swiper.slides.eq(swiper.activeIndex)[0]);
+    };
+
+    if (typeof speed === 'number') {
+      swiper.setTransition(speed);
+    } else if (speed === true) {
+      swiper.setTransition(swiper.params.speed);
     }
 
-    // Find new height from highest slide in view
-    for (i = 0; i < activeSlides.length; i += 1) {
-      if (typeof activeSlides[i] !== 'undefined') {
-        var height = activeSlides[i].offsetHeight;
-        newHeight = height > newHeight ? height : newHeight;
-      }
+    // Find slides currently in view
+    var activeSlides = [swiper.slides.eq(swiper.activeIndex)[0]];
+    if (
+      swiper.params.slidesPerView !== 'auto'
+      && swiper.params.slidesPerView > 1
+    ) {
+      activeSlides = swiper.params.centeredSlides
+        ? getActiveSlidesWhenCentered()
+        : getActiveSlides();
     }
+
+    // Find new max height from highest slide in view
+    var maxHeight = activeSlides.reduce(function (accum, slide) {
+      return accum < slide.offsetHeight
+        ? slide.offsetHeight
+        : accum;
+    }, 0);
 
     // Update Height
-    if (newHeight) { swiper.$wrapperEl.css('height', (newHeight + "px")); }
+    if (maxHeight) { swiper.$wrapperEl.css('height', (maxHeight + "px")); }
   }
 
   function updateSlidesOffset () {
