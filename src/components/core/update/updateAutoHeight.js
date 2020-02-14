dcbc65424471c4710a8160bd8d0b40f279402358
Tim Paul <tim.paul@immutable.com>
2020-02-14 14:56:06 +1100

    fix code comment typo

--- src/components/core/update/updateAutoHeight.js
+++ src/components/core/update/updateAutoHeight.js
@@ -18,7 +18,7 @@ export default function (speed) {
       Math.ceil(swiper.params.slidesPerView) / 2
     );
 
-    // Then get each slide inwards from the bouncds of the view:
+    // Then get each slide inwards from the bounds of the view:
     while (amountOfSlidesOnEachSide > 0) {
       const nextSlide = slidesArray[
         centerSlideIndex + amountOfSlidesOnEachSide
