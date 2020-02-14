fb3d25a9b28ef8d6fa2387cefc0d67f132233334
Tim Paul <tim.paul@immutable.com>
2020-02-14 12:56:23 +1100

    fix eslint complaints

--- src/components/core/update/updateAutoHeight.js
+++ src/components/core/update/updateAutoHeight.js
@@ -1,8 +1,8 @@
-export default function(speed) {
+export default function (speed) {
   const swiper = this;
   let newHeight = 0;
   let i;
-  if (typeof speed === "number") {
+  if (typeof speed === 'number') {
     swiper.setTransition(speed);
   } else if (speed === true) {
     swiper.setTransition(swiper.params.speed);
@@ -12,14 +12,14 @@ export default function(speed) {
   let activeSlides = [];
   if (swiper.params.slidesPerView > 1 && swiper.params.centeredSlides) {
     activeSlides = swiper.slides.filter(
-      (index, slide) =>
-        slide.classList.contains("swiper-slide-active") ||
-        slide.classList.contains("swiper-slide-prev") ||
-        slide.classList.contains("swiper-slide-next")
+      (index, slide) => (
+        slide.classList.contains('swiper-slide-active')
+        || slide.classList.contains('swiper-slide-prev')
+        || slide.classList.contains('swiper-slide-next'))
     );
   } else if (
-    swiper.params.slidesPerView !== "auto" &&
-    swiper.params.slidesPerView > 1
+    swiper.params.slidesPerView !== 'auto'
+    && swiper.params.slidesPerView > 1
   ) {
     for (i = 0; i < Math.ceil(swiper.params.slidesPerView); i += 1) {
       const index = swiper.activeIndex + i;
@@ -32,12 +32,12 @@ export default function(speed) {
 
   // Find new height from highest slide in view
   for (i = 0; i < activeSlides.length; i += 1) {
-    if (typeof activeSlides[i] !== "undefined") {
+    if (typeof activeSlides[i] !== 'undefined') {
       const height = activeSlides[i].offsetHeight;
       newHeight = height > newHeight ? height : newHeight;
     }
   }
 
   // Update Height
-  if (newHeight) swiper.$wrapperEl.css("height", `${newHeight}px`);
+  if (newHeight) swiper.$wrapperEl.css('height', `${newHeight}px`);
 }
