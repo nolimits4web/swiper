f3327ad7ff1e49d9bd203da0bf6252353571385e
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-05-23 11:47:06 +0300

    Remove old build

--- package/src/components/core/manipulation/removeAllSlides.js
+++
@@ -1,9 +0,0 @@
-export default function () {
-  const swiper = this;
-
-  const slidesIndexes = [];
-  for (let i = 0; i < swiper.slides.length; i += 1) {
-    slidesIndexes.push(i);
-  }
-  swiper.removeSlide(slidesIndexes);
-}
