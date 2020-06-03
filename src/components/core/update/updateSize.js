b26650e10cef116b87f6ee37f2797d39a4292387
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-06-03 12:33:50 +0300

    Fix NaN sizes in SSR

--- src/components/core/update/updateSize.js
+++ src/components/core/update/updateSize.js
@@ -5,12 +5,12 @@ export default function updateSize() {
   let width;
   let height;
   const $el = swiper.$el;
-  if (typeof swiper.params.width !== 'undefined') {
+  if (typeof swiper.params.width !== 'undefined' && swiper.params.width !== null) {
     width = swiper.params.width;
   } else {
     width = $el[0].clientWidth;
   }
-  if (typeof swiper.params.height !== 'undefined') {
+  if (typeof swiper.params.height !== 'undefined' && swiper.params.width !== null) {
     height = swiper.params.height;
   } else {
     height = $el[0].clientHeight;
@@ -28,6 +28,9 @@ export default function updateSize() {
     height -
     parseInt($el.css('padding-top') || 0, 10) -
     parseInt($el.css('padding-bottom') || 0, 10);
+
+  if (Number.isNaN(width)) width = 0;
+  if (Number.isNaN(height)) height = 0;
 
   extend(swiper, {
     width,
