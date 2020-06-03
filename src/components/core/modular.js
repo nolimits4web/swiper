6580507a51683484cd47ef5d11a23a94b9eba459
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-06-03 14:53:26 +0300

    Remove unused modular instance

--- src/components/core/modular.js
+++ src/components/core/modular.js
@@ -19,17 +19,7 @@ export default {
     Object.keys(instance.modules).forEach((moduleName) => {
       const module = instance.modules[moduleName];
       const moduleParams = modulesParams[moduleName] || {};
-      // Extend instance methods and props
-      if (module.instance) {
-        Object.keys(module.instance).forEach((modulePropName) => {
-          const moduleProp = module.instance[modulePropName];
-          if (typeof moduleProp === 'function') {
-            instance[modulePropName] = moduleProp.bind(instance);
-          } else {
-            instance[modulePropName] = moduleProp;
-          }
-        });
-      }
+
       // Add event listeners
       if (module.on && instance.on) {
         Object.keys(module.on).forEach((moduleEventName) => {
