adbdd3d88ca69e3d43abd4a9e80eae127f23ec29
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-07-02 18:11:11 +0300

    React: Forward Refs for container and slide

--- src/react/swiper-slide.js
+++ src/react/swiper-slide.js
@@ -1,58 +1,56 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, forwardRef } from 'react';
 import { uniqueClasses } from './utils';
 
-const SwiperSlide = ({
-  tag: Tag = 'div',
-  children,
-  className = '',
-  swiper,
-  zoom,
-  ...rest
-} = {}) => {
-  const slideElRef = useRef(null);
-  const [slideClasses, setSlideClasses] = useState('swiper-slide');
+const SwiperSlide = forwardRef(
+  ({ tag: Tag = 'div', children, className = '', swiper, zoom, ...rest } = {}, externalRef) => {
+    const slideElRef = useRef(null);
+    const [slideClasses, setSlideClasses] = useState('swiper-slide');
 
-  function updateClasses(swiper, el, classNames) {
-    if (el === slideElRef.current) {
-      setSlideClasses(classNames);
-    }
-  }
-
-  useEffect(() => {
-    if (!slideElRef.current || !swiper) return;
-    if (swiper.destroyed) {
-      if (slideClasses !== 'swiper-slide') {
-        setSlideClasses('swiper-slide');
+    function updateClasses(swiper, el, classNames) {
+      if (el === slideElRef.current) {
+        setSlideClasses(classNames);
       }
-      return;
     }
-    swiper.on('_slideClass', updateClasses);
-    // eslint-disable-next-line
-    return () => {
-      if (!swiper) return;
-      swiper.off('_slideClass', updateClasses);
-    };
-  });
 
-  return (
-    <Tag
-      ref={slideElRef}
-      className={uniqueClasses(`${slideClasses}${className ? ` ${className}` : ''}`)}
-      {...rest}
-    >
-      {zoom ? (
-        <div
-          className="swiper-zoom-container"
-          data-swiper-zoom={typeof zoom === 'number' ? zoom : undefined}
-        >
-          {children}
-        </div>
-      ) : (
-        children
-      )}
-    </Tag>
-  );
-};
+    useEffect(() => {
+      if (externalRef) {
+        externalRef.current = slideElRef.current;
+      }
+      if (!slideElRef.current || !swiper) return;
+      if (swiper.destroyed) {
+        if (slideClasses !== 'swiper-slide') {
+          setSlideClasses('swiper-slide');
+        }
+        return;
+      }
+      swiper.on('_slideClass', updateClasses);
+      // eslint-disable-next-line
+      return () => {
+        if (!swiper) return;
+        swiper.off('_slideClass', updateClasses);
+      };
+    });
+
+    return (
+      <Tag
+        ref={slideElRef}
+        className={uniqueClasses(`${slideClasses}${className ? ` ${className}` : ''}`)}
+        {...rest}
+      >
+        {zoom ? (
+          <div
+            className="swiper-zoom-container"
+            data-swiper-zoom={typeof zoom === 'number' ? zoom : undefined}
+          >
+            {children}
+          </div>
+        ) : (
+          children
+        )}
+      </Tag>
+    );
+  },
+);
 
 SwiperSlide.displayName = 'SwiperSlide';
 
--- src/react/swiper.js
+++ src/react/swiper.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, forwardRef } from 'react';
 import { getParams } from './get-params';
 import { initSwiper } from './init-swiper';
 import { needsScrollbar, needsNavigation, needsPagination, uniqueClasses } from './utils';
@@ -9,141 +9,152 @@ import { updateSwiper } from './update-swiper';
 import { renderVirtual, updateOnVirtualData } from './virtual';
 import { useIsomorphicLayoutEffect } from './use-isomorphic-layout-effect';
 
-const Swiper = ({
-  className,
-  tag: Tag = 'div',
-  wrapperTag: WrapperTag = 'div',
-  children,
-  onSwiper,
-  ...rest
-} = {}) => {
-  const [containerClasses, setContainerClasses] = useState('swiper-container');
-  const [virtualData, setVirtualData] = useState(null);
-  const initializedRef = useRef(false);
-  const swiperElRef = useRef(null);
-  const swiperRef = useRef(null);
-  const oldPassedParamsRef = useRef(null);
-  const oldSlides = useRef(null);
+const Swiper = forwardRef(
+  (
+    {
+      className,
+      tag: Tag = 'div',
+      wrapperTag: WrapperTag = 'div',
+      children,
+      onSwiper,
+      ...rest
+    } = {},
+    externalElRef,
+  ) => {
+    const [containerClasses, setContainerClasses] = useState('swiper-container');
+    const [virtualData, setVirtualData] = useState(null);
+    const initializedRef = useRef(false);
+    const swiperElRef = useRef(null);
+    const swiperRef = useRef(null);
+    const oldPassedParamsRef = useRef(null);
+    const oldSlides = useRef(null);
 
-  const nextElRef = useRef(null);
-  const prevElRef = useRef(null);
-  const paginationElRef = useRef(null);
-  const scrollbarElRef = useRef(null);
+    const nextElRef = useRef(null);
+    const prevElRef = useRef(null);
+    const paginationElRef = useRef(null);
+    const scrollbarElRef = useRef(null);
 
-  const { params: swiperParams, passedParams, rest: restProps } = getParams(rest);
+    const { params: swiperParams, passedParams, rest: restProps } = getParams(rest);
 
-  const { slides, slots } = getChildren(children);
+    const { slides, slots } = getChildren(children);
 
-  const changedParams = getChangedParams(
-    passedParams,
-    oldPassedParamsRef.current,
-    slides,
-    oldSlides.current,
-  );
-
-  oldPassedParamsRef.current = passedParams;
-  oldSlides.current = slides;
-
-  Object.assign(swiperParams.on, {
-    _containerClasses(swiper, classes) {
-      setContainerClasses(classes);
-    },
-    _swiper(swiper) {
-      swiper.loopCreate = () => {};
-      swiper.loopDestroy = () => {};
-      if (swiperParams.loop) {
-        swiper.loopedSlides = calcLoopedSlides(slides, swiperParams);
-      }
-      swiperRef.current = swiper;
-      if (swiper.virtual && swiper.params.virtual.enabled) {
-        swiper.virtual.slides = slides;
-        swiper.params.virtual.cache = false;
-        swiper.params.virtual.renderExternal = setVirtualData;
-        swiper.params.virtual.renderExternalUpdate = false;
-      }
-    },
-  });
-
-  // set initialized flag
-  useEffect(() => {
-    if (!initializedRef.current && swiperRef.current) {
-      swiperRef.current.emitSlidesClasses();
-      initializedRef.current = true;
-    }
-  });
-
-  // watch for params change
-  useIsomorphicLayoutEffect(() => {
-    if (changedParams.length && swiperRef.current && !swiperRef.current.destroyed) {
-      updateSwiper(swiperRef.current, slides, passedParams, changedParams);
-    }
-  });
-
-  // update on virtual update
-  useIsomorphicLayoutEffect(() => {
-    updateOnVirtualData(swiperRef.current);
-  }, [virtualData]);
-
-  // init swiper
-  useIsomorphicLayoutEffect(() => {
-    if (!swiperElRef.current) return;
-    initSwiper(
-      {
-        el: swiperElRef.current,
-        nextEl: nextElRef.current,
-        prevEl: prevElRef.current,
-        paginationEl: paginationElRef.current,
-        scrollbarEl: scrollbarElRef.current,
-      },
-      swiperParams,
+    const changedParams = getChangedParams(
+      passedParams,
+      oldPassedParamsRef.current,
+      slides,
+      oldSlides.current,
     );
 
-    if (onSwiper) onSwiper(swiperRef.current);
-    // eslint-disable-next-line
-    return () => {
-      if (swiperRef.current && !swiperRef.current.destroyed) {
-        swiperRef.current.destroy();
+    oldPassedParamsRef.current = passedParams;
+    oldSlides.current = slides;
+
+    Object.assign(swiperParams.on, {
+      _containerClasses(swiper, classes) {
+        setContainerClasses(classes);
+      },
+      _swiper(swiper) {
+        swiper.loopCreate = () => {};
+        swiper.loopDestroy = () => {};
+        if (swiperParams.loop) {
+          swiper.loopedSlides = calcLoopedSlides(slides, swiperParams);
+        }
+        swiperRef.current = swiper;
+        if (swiper.virtual && swiper.params.virtual.enabled) {
+          swiper.virtual.slides = slides;
+          swiper.params.virtual.cache = false;
+          swiper.params.virtual.renderExternal = setVirtualData;
+          swiper.params.virtual.renderExternalUpdate = false;
+        }
+      },
+    });
+
+    // set initialized flag
+    useEffect(() => {
+      if (!initializedRef.current && swiperRef.current) {
+        swiperRef.current.emitSlidesClasses();
+        initializedRef.current = true;
       }
-    };
-  }, []);
+    });
 
-  // bypass swiper instance to slides
-  function renderSlides() {
-    if (swiperParams.virtual) {
-      return renderVirtual(swiperRef.current, slides, virtualData);
-    }
-    if (!swiperParams.loop || (swiperRef.current && swiperRef.current.destroyed)) {
-      return slides.map((child) => {
-        return React.cloneElement(child, { swiper: swiperRef.current });
-      });
-    }
-    return renderLoop(swiperRef.current, slides, swiperParams);
-  }
+    // watch for params change
+    useIsomorphicLayoutEffect(() => {
+      if (changedParams.length && swiperRef.current && !swiperRef.current.destroyed) {
+        updateSwiper(swiperRef.current, slides, passedParams, changedParams);
+      }
+    });
 
-  return (
-    <Tag
-      ref={swiperElRef}
-      className={uniqueClasses(`${containerClasses}${className ? ` ${className}` : ''}`)}
-      {...restProps}
-    >
-      {slots['container-start']}
-      {needsNavigation(swiperParams) && (
-        <>
-          <div ref={prevElRef} className="swiper-button-prev" />
-          <div ref={nextElRef} className="swiper-button-next" />
-        </>
-      )}
-      {needsScrollbar(swiperParams) && <div ref={scrollbarElRef} className="swiper-scrollbar" />}
-      {needsPagination(swiperParams) && <div ref={paginationElRef} className="swiper-pagination" />}
-      <WrapperTag className="swiper-wrapper">
-        {slots['wrapper-start']}
-        {renderSlides()}
-        {slots['wrapper-end']}
-      </WrapperTag>
-      {slots['container-end']}
-    </Tag>
-  );
-};
+    // update on virtual update
+    useIsomorphicLayoutEffect(() => {
+      updateOnVirtualData(swiperRef.current);
+    }, [virtualData]);
+
+    // init swiper
+    useIsomorphicLayoutEffect(() => {
+      if (externalElRef) {
+        externalElRef.current = swiperElRef.current;
+      }
+      if (!swiperElRef.current) return;
+
+      initSwiper(
+        {
+          el: swiperElRef.current,
+          nextEl: nextElRef.current,
+          prevEl: prevElRef.current,
+          paginationEl: paginationElRef.current,
+          scrollbarEl: scrollbarElRef.current,
+        },
+        swiperParams,
+      );
+
+      if (onSwiper) onSwiper(swiperRef.current);
+      // eslint-disable-next-line
+      return () => {
+        if (swiperRef.current && !swiperRef.current.destroyed) {
+          swiperRef.current.destroy();
+        }
+      };
+    }, []);
+
+    // bypass swiper instance to slides
+    function renderSlides() {
+      if (swiperParams.virtual) {
+        return renderVirtual(swiperRef.current, slides, virtualData);
+      }
+      if (!swiperParams.loop || (swiperRef.current && swiperRef.current.destroyed)) {
+        return slides.map((child) => {
+          return React.cloneElement(child, { swiper: swiperRef.current });
+        });
+      }
+      return renderLoop(swiperRef.current, slides, swiperParams);
+    }
+
+    return (
+      <Tag
+        ref={swiperElRef}
+        className={uniqueClasses(`${containerClasses}${className ? ` ${className}` : ''}`)}
+        {...restProps}
+      >
+        {slots['container-start']}
+        {needsNavigation(swiperParams) && (
+          <>
+            <div ref={prevElRef} className="swiper-button-prev" />
+            <div ref={nextElRef} className="swiper-button-next" />
+          </>
+        )}
+        {needsScrollbar(swiperParams) && <div ref={scrollbarElRef} className="swiper-scrollbar" />}
+        {needsPagination(swiperParams) && (
+          <div ref={paginationElRef} className="swiper-pagination" />
+        )}
+        <WrapperTag className="swiper-wrapper">
+          {slots['wrapper-start']}
+          {renderSlides()}
+          {slots['wrapper-end']}
+        </WrapperTag>
+        {slots['container-end']}
+      </Tag>
+    );
+  },
+);
 
 Swiper.displayName = 'Swiper';
 
