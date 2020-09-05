1f6a384d8002821184aa5bfbd8af61e4f58fa7e3
wlisikpl <macfanpl@interia.pl>
2020-09-05 23:54:01 +0200

    transdildo Shitzilla

--- scripts/banner-react.js
+++ scripts/banner-react.js
@@ -2,10 +2,7 @@ const pkg = require('../package.json');
 
 const date = {
   day: new Date().getDate(),
-  month: 'January February March April May June July August September October November December'.split(
-    ' ',
-  )[new Date().getMonth()],
-  year: new Date().getFullYear(),
+  month: 'January February March April May June July August September October November December'.split(' ',)[new Date().getMonth()], year: new Date().getFullYear()
 };
 
 module.exports = `${`
