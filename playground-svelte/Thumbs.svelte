c250c567fd3372ab130d513235c4e79042fbbcdd
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-09-24 20:17:11 +0300

    Svelte demos

---
+++ playground-svelte/Thumbs.svelte
@@ -0,0 +1,42 @@
+<main>
+  <Swiper
+    thumbs={{ swiper: thumbsSwiper }}
+    slidesPerView={1}
+    spaceBetween={50}
+    navigation
+  >
+    {#each slides as slide, index (index)}
+      <SwiperSlide>Slide {slide + 1}</SwiperSlide>
+    {/each}
+  </Swiper>
+  <Swiper
+    on:swiper={setThumbsSwiper}
+    slidesPerView={5}
+    spaceBetween={10}
+    navigation
+    watchSlidesVisibility
+    watchSlidesProgress
+  >
+    {#each slides as slide, index (index)}
+      <SwiperSlide>Slide {slide + 1}</SwiperSlide>
+    {/each}
+  </Swiper>
+
+</main>
+<script>
+  import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Thumbs } from '../build/core';
+  import { Swiper, SwiperSlide } from '../build/svelte';
+
+  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Thumbs]);
+
+  let thumbsSwiper = null;
+
+  const slides = [0, 1, 2, 3, 4, 5, 6, 7];
+
+  const setThumbsSwiper = (e) => {
+    const [swiper] = e.detail;
+    setTimeout(() => {
+      thumbsSwiper = swiper;
+    })
+  }
+</script>
---
+++ playground-svelte/Virtual.svelte
@@ -0,0 +1,29 @@
+<main>
+  <Swiper
+    on:swiper={(e) => (window.swiper = e.detail[0])}
+    slidesPerView={3}
+    spaceBetween={50}
+    navigation
+    scrollbar={{ draggable: true }}
+    pagination={{ clickable: true }}
+    virtual={{
+      slides: virtualSlides,
+    }}
+    let:virtualData={{slides, offset, from}}
+  >
+    {#each slides as slide, index (from + index)}
+      <SwiperSlide
+        virtualIndex={from + index}
+        style={`left: ${offset}px`}
+      >{slide}</SwiperSlide>
+    {/each}
+  </Swiper>
+</main>
+<script>
+  import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Virtual } from '../build/core';
+  import { Swiper, SwiperSlide } from '../build/svelte';
+
+  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Virtual]);
+
+  const virtualSlides = Array.from({length: 5}).map((slides, index) => `Slide ${index + 1}`);
+</script>
