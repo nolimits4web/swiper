f883d9db90800437279d2574c7fbd7c677d10d35
Vlad <vl.tansky@gmail.com>
2020-12-11 19:28:12 +0200

    fix: postinstall echo

---
+++ package/postinstall.js
@@ -0,0 +1,2 @@
+console.log('\u001b[35m\u001b[1m','Love Swiper? Support Vladimir\'s work by donating or pledging on patreon:');
+console.log('\u001b[22m\u001b[39m\u001b[32m','> https://patreon.com/vladimirkharlampidi');
