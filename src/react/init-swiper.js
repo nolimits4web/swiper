71e3d15bd5f8375304a0125b23f2b7f0187c910b
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-01-25 18:20:35 +0300

    react: create swiper instance, and init when mounted

--- src/react/init-swiper.js
+++ src/react/init-swiper.js
@@ -2,27 +2,27 @@
 import Swiper from '../../core';
 import { needsNavigation, needsPagination, needsScrollbar } from './utils';
 
-function initSwiper({ el, nextEl, prevEl, paginationEl, scrollbarEl }, swiperParams) {
-  if (needsNavigation(swiperParams) && nextEl && prevEl) {
-    if (swiperParams.navigation === true) {
-      swiperParams.navigation = {};
-    }
-    swiperParams.navigation.nextEl = nextEl;
-    swiperParams.navigation.prevEl = prevEl;
-  }
-  if (needsPagination(swiperParams) && paginationEl) {
-    if (swiperParams.pagination === true) {
-      swiperParams.pagination = {};
-    }
-    swiperParams.pagination.el = paginationEl;
-  }
-  if (needsScrollbar(swiperParams) && scrollbarEl) {
-    if (swiperParams.scrollbar === true) {
-      swiperParams.scrollbar = {};
-    }
-    swiperParams.scrollbar.el = scrollbarEl;
-  }
-  return new Swiper(el, swiperParams);
+function initSwiper(swiperParams) {
+  return new Swiper(swiperParams);
 }
 
-export { initSwiper };
+function mountSwiper({ el, nextEl, prevEl, paginationEl, scrollbarEl, swiperRef }, swiperParams) {
+  const swiper = swiperRef.current;
+  if (needsNavigation(swiperParams) && nextEl && prevEl) {
+    swiper.params.navigation.nextEl = nextEl;
+    swiper.originalParams.navigation.nextEl = nextEl;
+    swiper.params.navigation.prevEl = prevEl;
+    swiper.originalParams.navigation.prevEl = prevEl;
+  }
+  if (needsPagination(swiperParams) && paginationEl) {
+    swiper.params.pagination.el = paginationEl;
+    swiper.originalParams.pagination.el = paginationEl;
+  }
+  if (needsScrollbar(swiperParams) && scrollbarEl) {
+    swiper.params.scrollbar.el = scrollbarEl;
+    swiper.originalParams.scrollbar.el = scrollbarEl;
+  }
+  swiperRef.current.init(el);
+}
+
+export { initSwiper, mountSwiper };
