e0d190975c6eaf8ad8ba5d7bac14cdd857a44181
Vlad <vl.tansky@gmail.com>
2021-02-16 17:13:20 +0200

    refactor(core): addClasses minify

--- src/components/core/classes/addClasses.js
+++ src/components/core/classes/addClasses.js
@@ -1,45 +1,38 @@
+function pushClasses(settings, containerModifierClass) {
+  const result = [];
+  settings.forEach((item) => {
+    if (typeof item === 'object') {
+      for (const [condition, classNames] of Object.entries(item)) {
+        if (condition) {
+          result.push(containerModifierClass + classNames);
+        }
+      }
+    }
+    if (typeof value === 'string') {
+      result.push(containerModifierClass + item);
+    }
+  });
+  return result;
+}
+
 export default function addClasses() {
   const swiper = this;
   const { classNames, params, rtl, $el, device, support } = swiper;
-  const suffixes = [];
+  // prettier-ignore
+  const suffixes = pushClasses([
+    'initialized',
+    params.direction,
+    { 'pointer-events': support.pointerEvents && !support.touch },
+    { 'free-mode': params.freeMode },
+    { 'autoheight': params.autoHeight },
+    { 'rtl': rtl },
+    { 'multirow': params.slidesPerColumn > 1 },
+    { 'multirow-column': params.slidesPerColumn > 1 && params.slidesPerColumnFill === 'column' },
+    { 'android': device.android },
+    { 'ios': device.ios },
+    { 'css-mode': params.cssMode },
+  ], params.containerModifierClass);
 
-  suffixes.push('initialized');
-  suffixes.push(params.direction);
-
-  if (support.pointerEvents && !support.touch) {
-    suffixes.push('pointer-events');
-  }
-  if (params.freeMode) {
-    suffixes.push('free-mode');
-  }
-  if (params.autoHeight) {
-    suffixes.push('autoheight');
-  }
-  if (rtl) {
-    suffixes.push('rtl');
-  }
-  if (params.slidesPerColumn > 1) {
-    suffixes.push('multirow');
-    if (params.slidesPerColumnFill === 'column') {
-      suffixes.push('multirow-column');
-    }
-  }
-  if (device.android) {
-    suffixes.push('android');
-  }
-  if (device.ios) {
-    suffixes.push('ios');
-  }
-
-  if (params.cssMode) {
-    suffixes.push('css-mode');
-  }
-
-  suffixes.forEach((suffix) => {
-    classNames.push(params.containerModifierClass + suffix);
-  });
-
-  $el.addClass(classNames.join(' '));
-
+  $el.addClass([...classNames, ...suffixes].join(' '));
   swiper.emitContainerClasses();
 }
