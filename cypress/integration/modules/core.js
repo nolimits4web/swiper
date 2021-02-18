51d06bc56145e6bd856a5a04feb10467fb348e08
Vlad <vl.tansky@gmail.com>
2021-02-18 19:10:12 +0200

    tests: uncomment core tests

--- cypress/integration/modules/core.js
+++ cypress/integration/modules/core.js
@@ -1,57 +1,57 @@
 /// <reference types="cypress" />
 
 context('Core', () => {
-  // describe('centeredSlides & slidesPerView', () => {
-  //   beforeEach(() => {
-  //     cy.swiperPage();
-  //     cy.initSwiper({
-  //       centeredSlides: true,
-  //       slidesPerView: 3,
-  //       spaceBetween: 10,
-  //       navigation: true,
-  //     });
-  //   });
-  //   it('should have slide with active class', () => {
-  //     cy.getSlide(0).should('have.class', 'swiper-slide-active').and('be.visible');
-  //   });
+  describe('centeredSlides & slidesPerView', () => {
+    beforeEach(() => {
+      cy.swiperPage();
+      cy.initSwiper({
+        centeredSlides: true,
+        slidesPerView: 3,
+        spaceBetween: 10,
+        navigation: true,
+      });
+    });
+    it('should have slide with active class', () => {
+      cy.getSlide(0).should('have.class', 'swiper-slide-active').and('be.visible');
+    });
 
-  //   it('should position slides center', () => {
-  //     cy.getActiveSlide().should(($el) => {
-  //       expect($el[0].getBoundingClientRect().x).to.be.greaterThan(100);
-  //     });
-  //   });
+    it('should position slides center', () => {
+      cy.getActiveSlide().should(($el) => {
+        expect($el[0].getBoundingClientRect().x).to.be.greaterThan(100);
+      });
+    });
 
-  //   it('should not has slide with prev class', () => {
-  //     cy.getSlides().should('not.have.class', 'swiper-slide-prev');
-  //   });
+    it('should not has slide with prev class', () => {
+      cy.getSlides().should('not.have.class', 'swiper-slide-prev');
+    });
 
-  //   it('should have slide with next class', () => {
-  //     cy.getSlide(1).should('have.class', 'swiper-slide-next').and('be.visible');
-  //   });
-  //   describe('visible slides', () => {
-  //     it('should have 2 visible slides', () => {
-  //       cy.getSlides().filter(':visible').its('length').should('be.eq', 2);
-  //     });
-  //     it('should have 3 visible slides after navigation', () => {
-  //       cy.nextSlide();
-  //       cy.getSlides().filter(':visible').its('length').should('be.eq', 2);
-  //     });
-  //   });
+    it('should have slide with next class', () => {
+      cy.getSlide(1).should('have.class', 'swiper-slide-next').and('be.visible');
+    });
+    describe('visible slides', () => {
+      it('should have 2 visible slides', () => {
+        cy.getSlides().filter(':visible').its('length').should('be.eq', 2);
+      });
+      it('should have 3 visible slides after navigation', () => {
+        cy.nextSlide();
+        cy.getSlides().filter(':visible').its('length').should('be.eq', 2);
+      });
+    });
 
-  //   it('should have 10px margin on slides', () => {
-  //     cy.getSlides()
-  //       .should('have.attr', 'style')
-  //       .and('match', /margin-right:\s+10px/);
-  //   });
+    it('should have 10px margin on slides', () => {
+      cy.getSlides()
+        .should('have.attr', 'style')
+        .and('match', /margin-right:\s+10px/);
+    });
 
-  //   it('should slide next with swiping the left', () => {
-  //     cy.getSliderContainer()
-  //       .trigger('pointerdown', { which: 1, pageX: 100, pageY: 100, force: true })
-  //       .trigger('pointermove', { pageX: 50, pageY: 100, force: true })
-  //       .trigger('pointerup', { force: true });
-  //     cy.getSlide(1).should('have.class', 'swiper-slide-active');
-  //   });
-  // });
+    it('should slide next with swiping the left', () => {
+      cy.getSliderContainer()
+        .trigger('pointerdown', { which: 1, pageX: 100, pageY: 100, force: true })
+        .trigger('pointermove', { pageX: 50, pageY: 100, force: true })
+        .trigger('pointerup', { force: true });
+      cy.getSlide(1).should('have.class', 'swiper-slide-active');
+    });
+  });
 
   describe('methods', () => {
     beforeEach(() => {
