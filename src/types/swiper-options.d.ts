5909caf717445e7ba992d4e8fe02217f7bc79b8a
DAnn2012 <dannunziata@hotmail.it>
2021-06-10 11:51:41 +0200

    Update swiper-options.d.ts

--- src/types/swiper-options.d.ts
+++ src/types/swiper-options.d.ts
@@ -1094,7 +1094,7 @@ export interface SwiperOptions {
   navigation?: NavigationOptions | boolean;
 
   /**
-   * Object with navigation parameters
+   * Object with pagination parameters
    *
    * @example
    * ```js
