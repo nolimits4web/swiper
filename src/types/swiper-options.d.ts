ab7dda5a9282c8446689220fe6ecda31d1ca2e85
DAnn2012 <dannunziata@hotmail.it>
2021-06-11 11:14:06 +0200

    Update swiper-options.d.ts

--- src/types/swiper-options.d.ts
+++ src/types/swiper-options.d.ts
@@ -946,6 +946,7 @@ export interface SwiperOptions {
    * @example
    * ```js
    * const swiper = new Swiper('.swiper-container', {
+   *   effect: 'coverflow',
    *   coverflowEffect: {
    *     rotate: 30,
    *     slideShadows: false,
@@ -961,6 +962,7 @@ export interface SwiperOptions {
    * @example
    * ```js
    * const swiper = new Swiper('.swiper-container', {
+   *   effect: 'cube',
    *   cubeEffect: {
    *     slideShadows: false,
    *   },
@@ -990,6 +992,7 @@ export interface SwiperOptions {
    * @example
    * ```js
    * const swiper = new Swiper('.swiper-container', {
+   *   effect: 'flip',
    *   flipEffect: {
    *     slideShadows: false,
    *   },
