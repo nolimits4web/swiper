89b584d5091ad8c60e3defcdf78717835584daf6
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-08-03 11:57:45 +0300

    chore: playground

--- playground/core/main.js
+++ playground/core/main.js
@@ -14,19 +14,4 @@ window.swiper = new Swiper({
   pagination: true,
   navigation: true,
   spaceBetween: 20,
-  breakpoints: {
-    640: {
-      slidesPerColumn: 2,
-    },
-    768: {
-      slidesPerColumn: 3,
-    },
-  },
-});
-
-// eslint-disable-next-line no-restricted-globals
-document.querySelector('.append-slide').addEventListener('click', (e) => {
-  e.preventDefault();
-  // eslint-disable-next-line no-restricted-globals
-  window.swiper.appendSlide('<div class="swiper-slide">Slide</div>');
 });
