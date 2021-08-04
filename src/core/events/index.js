9cc1a2967a666a704003fc39eb07fdec2233960c
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-08-04 17:00:01 +0300

    chore(core): refactor events/index

--- src/core/events/index.js
+++ src/core/events/index.js
@@ -10,58 +10,45 @@ import onScroll from './onScroll.js';
 let dummyEventAttached = false;
 function dummyEventListener() {}
 
-function attachEvents() {
-  const swiper = this;
+const events = (swiper, method) => {
   const document = getDocument();
   const { params, touchEvents, el, wrapperEl, device, support } = swiper;
-
-  swiper.onTouchStart = onTouchStart.bind(swiper);
-  swiper.onTouchMove = onTouchMove.bind(swiper);
-  swiper.onTouchEnd = onTouchEnd.bind(swiper);
-  if (params.cssMode) {
-    swiper.onScroll = onScroll.bind(swiper);
-  }
-
-  swiper.onClick = onClick.bind(swiper);
-
   const capture = !!params.nested;
+  const domMethod = method === 'on' ? 'addEventListener' : 'removeEventListener';
+  const swiperMethod = method;
 
   // Touch Events
   if (!support.touch) {
-    el.addEventListener(touchEvents.start, swiper.onTouchStart, false);
-    document.addEventListener(touchEvents.move, swiper.onTouchMove, capture);
-    document.addEventListener(touchEvents.end, swiper.onTouchEnd, false);
+    el[domMethod](touchEvents.start, swiper.onTouchStart, false);
+    document[domMethod](touchEvents.move, swiper.onTouchMove, capture);
+    document[domMethod](touchEvents.end, swiper.onTouchEnd, false);
   } else {
     const passiveListener =
       touchEvents.start === 'touchstart' && support.passiveListener && params.passiveListeners
         ? { passive: true, capture: false }
         : false;
-    el.addEventListener(touchEvents.start, swiper.onTouchStart, passiveListener);
-    el.addEventListener(
+    el[domMethod](touchEvents.start, swiper.onTouchStart, passiveListener);
+    el[domMethod](
       touchEvents.move,
       swiper.onTouchMove,
       support.passiveListener ? { passive: false, capture } : capture,
     );
-    el.addEventListener(touchEvents.end, swiper.onTouchEnd, passiveListener);
+    el[domMethod](touchEvents.end, swiper.onTouchEnd, passiveListener);
     if (touchEvents.cancel) {
-      el.addEventListener(touchEvents.cancel, swiper.onTouchEnd, passiveListener);
-    }
-    if (!dummyEventAttached) {
-      document.addEventListener('touchstart', dummyEventListener);
-      dummyEventAttached = true;
+      el[domMethod](touchEvents.cancel, swiper.onTouchEnd, passiveListener);
     }
   }
   // Prevent Links Clicks
   if (params.preventClicks || params.preventClicksPropagation) {
-    el.addEventListener('click', swiper.onClick, true);
+    el[domMethod]('click', swiper.onClick, true);
   }
   if (params.cssMode) {
-    wrapperEl.addEventListener('scroll', swiper.onScroll);
+    wrapperEl[domMethod]('scroll', swiper.onScroll);
   }
 
   // Resize handler
   if (params.updateOnWindowResize) {
-    swiper.on(
+    swiper[swiperMethod](
       device.ios || device.android
         ? 'resize orientationchange observerUpdate'
         : 'resize observerUpdate',
@@ -69,51 +56,36 @@ function attachEvents() {
       true,
     );
   } else {
-    swiper.on('observerUpdate', onResize, true);
+    swiper[swiperMethod]('observerUpdate', onResize, true);
   }
+};
+
+function attachEvents() {
+  const swiper = this;
+  const document = getDocument();
+  const { params, support } = swiper;
+
+  swiper.onTouchStart = onTouchStart.bind(swiper);
+  swiper.onTouchMove = onTouchMove.bind(swiper);
+  swiper.onTouchEnd = onTouchEnd.bind(swiper);
+
+  if (params.cssMode) {
+    swiper.onScroll = onScroll.bind(swiper);
+  }
+
+  swiper.onClick = onClick.bind(swiper);
+
+  if (support.touch && !dummyEventAttached) {
+    document.addEventListener('touchstart', dummyEventListener);
+    dummyEventAttached = true;
+  }
+
+  events(swiper, 'on');
 }
 
 function detachEvents() {
   const swiper = this;
-  const document = getDocument();
-
-  const { params, touchEvents, el, wrapperEl, device, support } = swiper;
-
-  const capture = !!params.nested;
-
-  // Touch Events
-  if (!support.touch) {
-    el.removeEventListener(touchEvents.start, swiper.onTouchStart, false);
-    document.removeEventListener(touchEvents.move, swiper.onTouchMove, capture);
-    document.removeEventListener(touchEvents.end, swiper.onTouchEnd, false);
-  } else {
-    const passiveListener =
-      touchEvents.start === 'onTouchStart' && support.passiveListener && params.passiveListeners
-        ? { passive: true, capture: false }
-        : false;
-    el.removeEventListener(touchEvents.start, swiper.onTouchStart, passiveListener);
-    el.removeEventListener(touchEvents.move, swiper.onTouchMove, capture);
-    el.removeEventListener(touchEvents.end, swiper.onTouchEnd, passiveListener);
-    if (touchEvents.cancel) {
-      el.removeEventListener(touchEvents.cancel, swiper.onTouchEnd, passiveListener);
-    }
-  }
-  // Prevent Links Clicks
-  if (params.preventClicks || params.preventClicksPropagation) {
-    el.removeEventListener('click', swiper.onClick, true);
-  }
-
-  if (params.cssMode) {
-    wrapperEl.removeEventListener('scroll', swiper.onScroll);
-  }
-
-  // Resize handler
-  swiper.off(
-    device.ios || device.android
-      ? 'resize orientationchange observerUpdate'
-      : 'resize observerUpdate',
-    onResize,
-  );
+  events(swiper, 'off');
 }
 
 export default {
