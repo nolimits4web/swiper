9c9970ca479f69d9fdebd1bedf830035b6b030e5
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-08-06 13:48:30 +0300

    feat(core): remove `isEdge` browser detection

--- src/shared/get-browser.js
+++ src/shared/get-browser.js
@@ -9,7 +9,6 @@ function calcBrowser() {
     return ua.indexOf('safari') >= 0 && ua.indexOf('chrome') < 0 && ua.indexOf('android') < 0;
   }
   return {
-    isEdge: !!window.navigator.userAgent.match(/Edge/g),
     isSafari: isSafari(),
     isWebView: /(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(window.navigator.userAgent),
   };
