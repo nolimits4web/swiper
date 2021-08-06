cab18b4b108deca159432db4da6dea007cc783f0
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-08-06 13:48:41 +0300

    chore(core): optimization

--- src/core/transition/transitionStart.js
+++ src/core/transition/transitionStart.js
@@ -1,30 +1,12 @@
+import transitionEmit from './transitionEmit.js';
+
 export default function transitionStart(runCallbacks = true, direction) {
   const swiper = this;
-  const { activeIndex, params, previousIndex } = swiper;
+  const { params } = swiper;
   if (params.cssMode) return;
   if (params.autoHeight) {
     swiper.updateAutoHeight();
   }
 
-  let dir = direction;
-  if (!dir) {
-    if (activeIndex > previousIndex) dir = 'next';
-    else if (activeIndex < previousIndex) dir = 'prev';
-    else dir = 'reset';
-  }
-
-  swiper.emit('transitionStart');
-
-  if (runCallbacks && activeIndex !== previousIndex) {
-    if (dir === 'reset') {
-      swiper.emit('slideResetTransitionStart');
-      return;
-    }
-    swiper.emit('slideChangeTransitionStart');
-    if (dir === 'next') {
-      swiper.emit('slideNextTransitionStart');
-    } else {
-      swiper.emit('slidePrevTransitionStart');
-    }
-  }
+  transitionEmit({ swiper, runCallbacks, direction, step: 'Start' });
 }
