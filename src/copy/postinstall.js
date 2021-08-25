e54f07d6047f56e0469fd7d3024ede36e5b942fe
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-08-25 14:06:27 +0300

    Update postinstall.js

--- src/copy/postinstall.js
+++ src/copy/postinstall.js
@@ -17,7 +17,7 @@ var CI = [
 ].some(function (it) { return is(env[it]); });
 
 var BANNER = '\u001b[35m\u001b[1mLove Swiper? Support Vladimir\'s work by donating or pledging: \u001B[0m\n' +
-'\u001b[22m\u001b[39m\u001b[32m> On Patreon https://patreon.com/vladimirkharlampidi \u001B[0m\n' +
+'\u001b[22m\u001b[39m\u001b[32m> On Patreon https://patreon.com/swiperjs \u001B[0m\n' +
 '\u001b[22m\u001b[39m\u001b[32m> On Open Collective https://opencollective.com/swiper';
 
 function is(it) {
