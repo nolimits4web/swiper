b4cdea842207f12603d09cbfa2828c66062273c3
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-09-08 18:47:12 +0300

    fix(core): don't auto create elements on destroy/init

--- src/shared/create-element-if-not-defined.js
+++ src/shared/create-element-if-not-defined.js
@@ -5,9 +5,12 @@ export default function createElementIfNotDefined(swiper, originalParams, params
   if (swiper.params.createElements) {
     Object.keys(checkProps).forEach((key) => {
       if (!params[key] && params.auto === true) {
-        const element = document.createElement('div');
-        element.className = checkProps[key];
-        swiper.$el.append(element);
+        let element = swiper.$el.children(`.${checkProps[key]}`)[0];
+        if (!element) {
+          element = document.createElement('div');
+          element.className = checkProps[key];
+          swiper.$el.append(element);
+        }
         params[key] = element;
         originalParams[key] = element;
       }
