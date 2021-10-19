6ac4c60c1b0f8e62673a343134b08c8a833413aa
Jusung Hwang(nabigraphics) <hbh2692@naver.com>
2021-10-20 00:08:03 +0900

    feat: Add transform with slidesOffsetBefore and offsetWidth

--- src/modules/effect-creative/effect-creative.js
+++ src/modules/effect-creative/effect-creative.js
@@ -32,9 +32,17 @@ export default function EffectCreative({ swiper, extendParams, on }) {
   };
 
   const setTranslate = () => {
-    const { slides } = swiper;
+    const { slides, $wrapperEl } = swiper;
     const params = swiper.params.creativeEffect;
     const { progressMultiplier: multiplier } = params;
+
+    const isCenteredSlides = swiper.params.centeredSlides;
+
+    if (isCenteredSlides) {
+      const margin = slides.eq(0)[0].offsetWidth / 2 - swiper.params.slidesOffsetBefore || 0;
+      $wrapperEl.transform(`translateX(calc(50% - ${margin}px))`);
+    }
+
     for (let i = 0; i < slides.length; i += 1) {
       const $slideEl = slides.eq(i);
       const slideProgress = $slideEl[0].progress;
