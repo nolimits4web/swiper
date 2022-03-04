95c882c50a798ebe7adc20fcb61765aace0edbfc
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-03-04 10:52:00 +0300

    fix(pagination): use direction classes
    fixes #5489

--- src/modules/pagination/pagination.js
+++ src/modules/pagination/pagination.js
@@ -300,7 +300,7 @@ export default function Pagination({ swiper, extendParams, on, emit }) {
     }
 
     $el.addClass(params.modifierClass + params.type);
-    $el.addClass(params.modifierClass + swiper.params.direction);
+    $el.addClass(swiper.isHorizontal() ? params.horizontalClass : params.verticalClass);
 
     if (params.type === 'bullets' && params.dynamicBullets) {
       $el.addClass(`${params.modifierClass}${params.type}-dynamic`);
@@ -338,7 +338,7 @@ export default function Pagination({ swiper, extendParams, on, emit }) {
 
     $el.removeClass(params.hiddenClass);
     $el.removeClass(params.modifierClass + params.type);
-    $el.removeClass(params.modifierClass + swiper.params.direction);
+    $el.removeClass(swiper.isHorizontal() ? params.horizontalClass : params.verticalClass);
     if (swiper.pagination.bullets && swiper.pagination.bullets.removeClass)
       swiper.pagination.bullets.removeClass(params.bulletActiveClass);
     if (params.clickable) {
