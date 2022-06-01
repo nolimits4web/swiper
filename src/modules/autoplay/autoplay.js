56e050bcdbcc00db7af02ccb32e0725265dd242a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-06-01 12:33:49 +0300

    Revert "fix(autoplay): fix swiper getting stuck with `0` autoplay delay"
    This reverts commit 61db26fdde129afc77bb56c302790c8af6f35c44.

--- src/modules/autoplay/autoplay.js
+++ src/modules/autoplay/autoplay.js
@@ -131,10 +131,6 @@ export default function Autoplay({ swiper, extendParams, on, emit }) {
     swiper.autoplay.paused = false;
     if (!swiper.autoplay.running) {
       stop();
-    } else if (swiper.params.autoplay.delay === 0 && swiper.animating) {
-      swiper.once('transitionEnd', () => {
-        run();
-      });
     } else {
       run();
     }
