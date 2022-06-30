eafa4598f135537d242232291dcc677e1342b348
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-06-30 11:50:26 +0300

    chore: test reactivity

--- playground/solid/App.jsx
+++ playground/solid/App.jsx
@@ -1,20 +1,24 @@
 /** @jsxImportSource solid-js */
 /* eslint-disable no-restricted-globals */
+import { createSignal } from 'solid-js';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/solid/swiper-solid';
 
 const App = () => {
+  const [enabled, toggle] = createSignal(true);
   return (
     <main>
+      <button onClick={() => toggle(!enabled())}>Toggle</button>
+      <p>Enabled: {String(enabled())}</p>
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         onSwiper={(swiper) => (window.swiper = swiper)}
-        slidesPerView={3}
-        spaceBetween={50}
-        navigation
+        slidesPerView={enabled() ? 3 : 1}
+        spaceBetween={enabled() ? 50 : 16}
+        navigation={enabled()}
         loop
-        scrollbar={{ draggable: true }}
-        pagination={{ clickable: true }}
+        scrollbar={enabled()}
+        pagination={enabled()}
       >
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
