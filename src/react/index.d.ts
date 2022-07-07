f6998d399a48195314d7bcb1d49202d31b6554ef
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-07-07 14:02:05 -0300

    Rename swiper-solid.js and swiper-react.js to index.js
    This way the following code is valid:
    ```js
    // With SolidJS
    import { Swiper } from "swiper/solid"
    // With React
    import { Swiper } from "swiper/react"
    ```

--- src/react/index.d.ts
+++ src/react/index.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { SwiperOptions, Swiper as SwiperClass } from '../types/';
+import { SwiperOptions, Swiper as SwiperClass } from '../types';
 
 interface SwiperProps extends SwiperOptions {
   /**
