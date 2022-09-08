de5de009c85cdbef1cb38d190185eeea1a1af7a8
0x0a0d <vandug23@gmail.com>
2022-09-08 14:12:02 +0700

    feat(build-config): allow to custom modules by process.env.SWIPER_BUILD_MODULES

--- scripts/utils/helper.js
+++ scripts/utils/helper.js
@@ -9,7 +9,9 @@ export function capitalizeString(str) {
  */
 export function parseSwiperBuildModulesEnv() {
   if (process.env.SWIPER_BUILD_MODULES) {
-    const buildModules = process.env.SWIPER_BUILD_MODULES.split(/[,\s]+/);
+    const buildModules = process.env.SWIPER_BUILD_MODULES.split(/[,\s]+/).filter(
+      (moduleName) => moduleName, // ensure to empty will be removed
+    );
     if (buildModules.length) return buildModules;
   }
   return null;
