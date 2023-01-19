3f4d1e5b73e66048bf39fcf1238c8299f9239281
Vladimir Kharlampidi <nolimits4web@gmail.com>
2023-01-19 14:45:49 +0300

    types

--- src/element/swiper-element.d.ts
+++ src/element/swiper-element.d.ts
@@ -1,5 +1,5 @@
 declare const SwiperContainer: HTMLElement;
 declare const SwiperSlide: HTMLElement;
-declare const register: (injectStyles: boolean) => void;
+declare const register: (injectStyles?: boolean) => void;
 
 export { register, SwiperContainer, SwiperSlide };
