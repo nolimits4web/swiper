85059be15f94c53c0fed637f94992a183358e54e
Derek Brooks <derek@broox.com>
2024-02-10 15:44:03 -0600

    Move click/tap events to onClick handler to fix bug with double tapping on mobile chrome.

--- src/core/events/onClick.mjs
+++ src/core/events/onClick.mjs
@@ -1,5 +1,9 @@
+import { now } from '../../shared/utils.mjs';
+
 export default function onClick(e) {
   const swiper = this;
+  const data = swiper.touchEventsData;
+
   if (!swiper.enabled) return;
   if (!swiper.allowClick) {
     if (swiper.params.preventClicks) e.preventDefault();
@@ -7,5 +11,18 @@ export default function onClick(e) {
       e.stopPropagation();
       e.stopImmediatePropagation();
     }
+    return;
   }
+
+  const clickTime = now();
+  const pathTree = e.path || e.composedPath?.();
+
+  swiper.updateClickedSlide((pathTree && pathTree[0]) || e.target, pathTree);
+  swiper.emit('tap click', e);
+
+  if (clickTime - data.lastClickTime < 300) {
+    swiper.emit('doubleTap doubleClick', e);
+  }
+
+  data.lastClickTime = now();
 }
--- src/core/events/onTouchEnd.mjs
+++ src/core/events/onTouchEnd.mjs
@@ -1,4 +1,4 @@
-import { now, nextTick } from '../../shared/utils.mjs';
+import { nextTick } from '../../shared/utils.mjs';
 
 export default function onTouchEnd(event) {
   const swiper = this;
@@ -55,21 +55,6 @@ export default function onTouchEnd(event) {
     swiper.setGrabCursor(false);
   }
 
-  // Time diff
-  const touchEndTime = now();
-  const timeDiff = touchEndTime - data.touchStartTime;
-
-  // Tap, doubleTap, Click
-  if (swiper.allowClick) {
-    const pathTree = e.path || (e.composedPath && e.composedPath());
-    swiper.updateClickedSlide((pathTree && pathTree[0]) || e.target, pathTree);
-    swiper.emit('tap click', e);
-    if (timeDiff < 300 && touchEndTime - data.lastClickTime < 300) {
-      swiper.emit('doubleTap doubleClick', e);
-    }
-  }
-
-  data.lastClickTime = now();
   nextTick(() => {
     if (!swiper.destroyed) swiper.allowClick = true;
   });
