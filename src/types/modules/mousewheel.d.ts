237e7c4fa99b816bae5ae100ba93894ea0146d71
Alex <lifecoder.ua@gmail.com>
2024-02-27 12:27:14 +0200

    Declare enabled option in mousewheel.d.ts (#7317)

--- src/types/modules/mousewheel.d.ts
+++ src/types/modules/mousewheel.d.ts
@@ -27,15 +27,19 @@ export interface MousewheelEvents {
 
 export interface MousewheelOptions {
   /**
+   * Set to `true` to enable mousewheel control
+   *
+   * @default false
+   */
+  enabled?: boolean;
+  /**
    * Set to `true` to force mousewheel swipes to axis. So in horizontal mode mousewheel will work only with horizontal mousewheel scrolling, and only with vertical scrolling in vertical mode.
-
    *
    * @default false
    */
   forceToAxis?: boolean;
   /**
    * Set to `true` and swiper will release mousewheel event and allow page scrolling when swiper is on edge positions (in the beginning or in the end)
-
    *
    * @default false
    */
