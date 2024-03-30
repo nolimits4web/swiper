4266bf8fbe9f081aaf6b8a6297b6d3778a52c0e0
Vida Xie <38204901+9romise@users.noreply.github.com>
2024-03-30 14:56:32 +0800

    fix(types): Update shared.d.ts

--- src/types/shared.d.ts
+++ src/types/shared.d.ts
@@ -3,11 +3,11 @@ import { Swiper } from './index.d.ts';
 export interface CSSSelector extends String {}
 
 export type SwiperModule = (options: {
-  params?: Swiper['params'];
-  swiper?: Swiper;
-  extendParams?: (obj: { [name: string]: any }) => void;
-  on?: Swiper['on'];
-  once?: Swiper['once'];
-  off?: Swiper['off'];
-  emit?: Swiper['emit'];
+  params: Swiper['params'];
+  swiper: Swiper;
+  extendParams: (obj: { [name: string]: any }) => void;
+  on: Swiper['on'];
+  once: Swiper['once'];
+  off: Swiper['off'];
+  emit: Swiper['emit'];
 }) => void;
