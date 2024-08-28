df957bbdb81ffeb7ef752598f740d4e155f9840a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2024-08-28 16:39:18 +0300

    fix(virtual): fix bypassing initial translate check if Virtual is enabled
    fixes #7699

--- src/core/slide/slideTo.mjs
+++ src/core/slide/slideTo.mjs
@@ -93,8 +93,14 @@ export default function slideTo(index = 0, speed, runCallbacks = true, internal,
   else if (slideIndex < activeIndex) direction = 'prev';
   else direction = 'reset';
 
+  // initial virtual
+  const isVirtual = swiper.virtual && swiper.params.virtual.enabled;
+  const isInitialVirtual = isVirtual && initial;
   // Update Index
-  if ((rtl && -translate === swiper.translate) || (!rtl && translate === swiper.translate)) {
+  if (
+    !isInitialVirtual &&
+    ((rtl && -translate === swiper.translate) || (!rtl && translate === swiper.translate))
+  ) {
     swiper.updateActiveIndex(slideIndex);
     // Update Height
     if (params.autoHeight) {
@@ -114,7 +120,6 @@ export default function slideTo(index = 0, speed, runCallbacks = true, internal,
     const isH = swiper.isHorizontal();
     const t = rtl ? translate : -translate;
     if (speed === 0) {
-      const isVirtual = swiper.virtual && swiper.params.virtual.enabled;
       if (isVirtual) {
         swiper.wrapperEl.style.scrollSnapType = 'none';
         swiper._immediateVirtual = true;
