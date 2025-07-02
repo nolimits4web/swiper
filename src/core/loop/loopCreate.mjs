e51a8b43a455e04603eb359b91277ced7ac7d746
Wessel Kroos <wessel_kroos@hotmail.com>
2025-07-02 22:56:39 +0200

    Fixes #7298 #7956 #6916 by making slidesOffsetBefore & slidesOffsetAfter work in combination with centeredSlides, slidesPerView & loop

--- src/core/loop/loopCreate.mjs
+++ src/core/loop/loopCreate.mjs
@@ -73,9 +73,10 @@ export default function loopCreate(slideRealIndex, initial) {
     initSlides();
   }
 
+  const bothDirections = (params.centeredSlides || !!params.slidesOffsetBefore || !!params.slidesOffsetAfter);
   swiper.loopFix({
     slideRealIndex,
-    direction: params.centeredSlides ? undefined : 'next',
+    direction: bothDirections ? undefined : 'next',
     initial,
   });
 }
