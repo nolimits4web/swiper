c97ae5d0069cf3d5c745efb63ced9924a64d2453
Vladimir Kharlampidi <nolimits4web@gmail.com>
2025-10-21 11:17:09 -0400

    feat(core): add 'getRotateFix' export to effect utils
    fixes #8114

--- src/swiper-effect-utils.mjs
+++ src/swiper-effect-utils.mjs
@@ -2,6 +2,13 @@ import createShadow from './shared/create-shadow.mjs';
 import effectInit from './shared/effect-init.mjs';
 import effectTarget from './shared/effect-target.mjs';
 import effectVirtualTransitionEnd from './shared/effect-virtual-transition-end.mjs';
-import { getSlideTransformEl } from './shared/utils.mjs';
+import { getSlideTransformEl, getRotateFix } from './shared/utils.mjs';
 
-export { effectInit, effectTarget, effectVirtualTransitionEnd, getSlideTransformEl, createShadow };
+export {
+  effectInit,
+  effectTarget,
+  effectVirtualTransitionEnd,
+  getSlideTransformEl,
+  createShadow,
+  getRotateFix,
+};
