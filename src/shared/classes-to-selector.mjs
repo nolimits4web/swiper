d35f41a85bd1793de58358d06300440e09187a6d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2026-01-28 13:57:40 -0500

    fix(core): Escape all CSS selector special characters
    fixes #8135

--- src/shared/classes-to-selector.mjs
+++ src/shared/classes-to-selector.mjs
@@ -1,6 +1,7 @@
 export default function classesToSelector(classes = '') {
+  // Escape all CSS selector special characters
   return `.${classes
     .trim()
-    .replace(/([\.:!+\/()[\]])/g, '\\$1') // eslint-disable-line
+    .replace(/([\.:!+\/()[\]#>~*^$|=,'"@{}\\])/g, '\\$1') // eslint-disable-line
     .replace(/ /g, '.')}`;
 }
