bae58ff02868646c8163925721b608fe03edd29f
Daniel Sattler <daniel.sattler@b13.com>
2026-04-07 15:53:44 +0200

    fix: remove aria-controls referencing shadow DOM id

--- src/modules/a11y/a11y.mjs
+++ src/modules/a11y/a11y.mjs
@@ -214,7 +214,6 @@ export default function A11y({ swiper, extendParams, on }) {
       el.addEventListener('keydown', onEnterOrSpaceKey);
     }
     addElLabel(el, message);
-    addElControls(el, wrapperId);
   };
 
   const handlePointerDown = (e) => {
