88ada42ac084571dd4c1017eb20e463c052f0b1e
Daniel Sattler <daniel.sattler@b13.com>
2026-04-07 15:29:03 +0200

    fix: remove redundant aria-disabled=false from swiper nav button

--- src/modules/a11y/a11y.mjs
+++ src/modules/a11y/a11y.mjs
@@ -105,7 +105,7 @@ export default function A11y({ swiper, extendParams, on }) {
   function enableEl(el) {
     el = makeElementsArray(el);
     el.forEach((subEl) => {
-      subEl.setAttribute('aria-disabled', false);
+      subEl.removeAttribute('aria-disabled');
     });
   }
 
